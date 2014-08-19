9a9db2134574450ee6fb47975d9dfdc97a162802
Ilia Naryzhny <phantom@ydn.ru>
2014-08-18 18:45:03 -0700

    Add profile to build standalone version for issue #8

---
+++ src/main/java/ru/ydn/orienteer/StartStandalone.java
@@ -0,0 +1,47 @@
+package ru.ydn.orienteer;
+
+import org.apache.wicket.util.time.Duration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class StartStandalone
+{
+    public static void main(String[] args) throws Exception {
+        int timeout = (int) Duration.ONE_HOUR.getMilliseconds();
+
+        Server server = new Server();
+        SocketConnector connector = new SocketConnector();
+
+        // Set some timeout options to make debugging easier.
+        connector.setMaxIdleTime(timeout);
+        connector.setSoLingerTime(-1);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        WebAppContext bb = new WebAppContext();
+        bb.setServer(server);
+        bb.setContextPath("/");
+        bb.setWar("src/main/webapp");
+
+        // START JMX SERVER
+        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+        // server.getContainer().addEventListener(mBeanContainer);
+        // mBeanContainer.start();
+
+        server.setHandler(bb);
+
+        try {
+            System.out.println("Starting Orienteer, PRESS ANY KEY TO STOP");
+            server.start();
+            System.in.read();
+            System.out.println("Stopping Orienteer");
+            server.stop();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
