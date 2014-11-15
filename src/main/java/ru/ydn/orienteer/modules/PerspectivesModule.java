df89a49cd061787c55336fc6d21ff37447ba0b8b
Ilia Naryzhny <phantom@ydn.ru>
2014-11-14 22:10:02 -0800

    Installation of default perspective for issue #6

--- src/main/java/ru/ydn/orienteer/modules/PerspectivesModule.java
+++ src/main/java/ru/ydn/orienteer/modules/PerspectivesModule.java
@@ -1,22 +1,30 @@
 package ru.ydn.orienteer.modules;
 
+import java.util.List;
+
 import ru.ydn.orienteer.CustomAttributes;
 import ru.ydn.orienteer.OrienteerWebApplication;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OClass.INDEX_TYPE;
 import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
 import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
 public class PerspectivesModule extends AbstractOrienteerModule
 {
 	public static final String OCLASS_PERSPECTIVE="OPerspective";
 	public static final String OCLASS_ITEM = "OPerspectiveItem";
+	
+	public static final String DEFAULT_PERSPECTIVE = "Default";
+	
 
 	public PerspectivesModule()
 	{
-		super("prespectives", 1);
+		super("perspectives", 1);
 	}
 
 	@Override
@@ -25,11 +33,13 @@ public class PerspectivesModule extends AbstractOrienteerModule
 		OClass perspectiveClass = mergeOClass(schema, OCLASS_PERSPECTIVE);
 		OClass itemClass = mergeOClass(schema, OCLASS_ITEM);
 		mergeOProperty(perspectiveClass, "name", OType.STRING);
+		mergeOProperty(perspectiveClass, "homeUrl", OType.STRING);
 		OProperty menu = mergeOProperty(perspectiveClass, "menu", OType.LINKLIST, "table").setLinkedClass(itemClass);
 		mergeOProperty(perspectiveClass, "footer", OType.STRING, "textarea");
 		assignNameAndParent(perspectiveClass, "name", null);
-		switchDisplayable(perspectiveClass, true, "name");
-		orderProperties(perspectiveClass, "name", "footer", "menu");
+		switchDisplayable(perspectiveClass, true, "name", "homeUrl");
+		orderProperties(perspectiveClass, "name", "homeUrl", "footer", "menu");
+		perspectiveClass.createIndex(OCLASS_PERSPECTIVE+".name", INDEX_TYPE.UNIQUE, "name");
 		
 		mergeOProperty(itemClass, "name", OType.STRING);
 		mergeOProperty(itemClass, "icon", OType.STRING);
@@ -41,6 +51,40 @@ public class PerspectivesModule extends AbstractOrienteerModule
 		
 		CustomAttributes.PROP_INVERSE.setValue(menu, perspective);
 		CustomAttributes.PROP_INVERSE.setValue(perspective, menu);
+		
+	}
+
+	@Override
+	public void onInitialize(OrienteerWebApplication app, ODatabaseDocument db) {
+		List<ODocument> defaultPerspectives = db.query(new OSQLSynchQuery<ODocument>("select from "+OCLASS_PERSPECTIVE+" where name=?"), DEFAULT_PERSPECTIVE);
+		if(defaultPerspectives==null || defaultPerspectives.size()<1)
+		{
+			ODocument perspective = new ODocument(OCLASS_PERSPECTIVE);
+			perspective.field("name", DEFAULT_PERSPECTIVE);
+			perspective.field("homeUrl", "/classes");
+			perspective.save();
+			
+			ODocument item = new ODocument(OCLASS_ITEM);
+			item.field("name", "Users");
+			item.field("icon", "users");
+			item.field("url", "/browse/OUser");
+			item.field("perspective", perspective);
+			item.save();
+			
+			item = new ODocument(OCLASS_ITEM);
+			item.field("name", "Roles");
+			item.field("icon", "users");
+			item.field("url", "/browse/ORole");
+			item.field("perspective", perspective);
+			item.save();
+			
+			item = new ODocument(OCLASS_ITEM);
+			item.field("name", "Classes");
+			item.field("icon", "cubes");
+			item.field("url", "/classes");
+			item.field("perspective", perspective);
+			item.save();
+		}
 	}
 
 }
