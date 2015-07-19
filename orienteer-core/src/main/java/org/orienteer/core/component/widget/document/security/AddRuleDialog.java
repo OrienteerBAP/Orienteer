01aad17ac5800b47c09a74cb32404b88b4db0737
Ilia Naryzhny <phantom@ydn.ru>
2015-07-18 23:32:50 -0700

    Dialog to add new orule: issue #98

---
+++ orienteer-core/src/main/java/org/orienteer/core/component/widget/document/security/AddRuleDialog.java
@@ -0,0 +1,45 @@
+package org.orienteer.core.component.widget.document.security;
+
+import java.util.Arrays;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+import com.orientechnologies.orient.core.metadata.security.ORule;
+
+/**
+ * Dialog for entering information about new {@link ORule}
+ */
+public abstract class AddRuleDialog extends Panel {
+	
+	private IModel<ORule.ResourceGeneric> resourceModel = Model.of();
+	private IModel<String> specificModel = Model.of();
+
+	public AddRuleDialog(String id) {
+		super(id);
+		Form<Object> form = new Form<Object>("form");
+		form.add(new DropDownChoice<ORule.ResourceGeneric>("resource", resourceModel, 
+								Arrays.asList(ORule.ResourceGeneric.values()), new ChoiceRenderer<ORule.ResourceGeneric>("name"))
+					.setNullValid(true));
+		form.add(new TextField<String>("specific", specificModel));
+		form.add(new AjaxButton("submit", form) {
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				onRuleEntered(target, resourceModel.getObject(), specificModel.getObject());
+			}
+		});
+		
+		add(form);
+	}
+	
+	protected abstract void onRuleEntered(AjaxRequestTarget target, ORule.ResourceGeneric resource, String specific);
+	
+}
--- orienteer-core/src/main/java/org/orienteer/core/component/widget/document/security/ORoleSecurityWidget.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/widget/document/security/ORoleSecurityWidget.java
@@ -3,12 +3,14 @@ package org.orienteer.core.component.widget.document.security;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.event.IEvent;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
@@ -22,9 +24,11 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
+import org.orienteer.core.component.BootstrapType;
 import org.orienteer.core.component.FAIcon;
 import org.orienteer.core.component.FAIconType;
 import org.orienteer.core.component.command.AbstractDeleteCommand;
+import org.orienteer.core.component.command.AbstractModalWindowCommand;
 import org.orienteer.core.component.command.AbstractSaveCommand;
 import org.orienteer.core.component.command.Command;
 import org.orienteer.core.component.property.BooleanEditPanel;
@@ -33,7 +37,10 @@ import org.orienteer.core.component.table.OrienteerDataTable;
 import org.orienteer.core.component.widget.oclass.OClassSecurityWidget;
 import org.orienteer.core.event.ActionPerformedEvent;
 import org.orienteer.core.widget.AbstractWidget;
+import org.orienteer.core.widget.DashboardPanel;
+import org.orienteer.core.widget.IWidgetType;
 import org.orienteer.core.widget.Widget;
+import org.orienteer.core.widget.command.modal.AddWidgetDialog;
 
 import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
 import ru.ydn.wicket.wicketorientdb.behavior.DisableIfDocumentNotSavedBehavior;
@@ -48,6 +55,7 @@ import com.google.common.base.Converter;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.security.ORole;
 import com.orientechnologies.orient.core.metadata.security.ORule;
+import com.orientechnologies.orient.core.metadata.security.ORule.ResourceGeneric;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 
 /**
@@ -127,6 +135,43 @@ public class ORoleSecurityWidget extends AbstractWidget<ODocument> {
 		JavaSortableDataProvider<String, String> provider = new JavaSortableDataProvider<String, String>(new PropertyModel<Collection<String>>(this, "ruleSet"));
 		OrienteerDataTable<String, String> sTable = new OrienteerDataTable<String, String>("table", sColumns, provider ,20);
 		sTable.addCommand(new AbstractSaveCommand<String>(sTable, null));
+		sTable.addCommand(new AbstractModalWindowCommand<String>(sTable.getCommandsToolbar().newChildId(), "command.add") {
+			{
+				setIcon(FAIconType.plus_circle);
+				setBootstrapType(BootstrapType.SUCCESS);
+			}
+
+			@Override
+			protected void initializeContent(final ModalWindow modal) {
+				modal.setTitle(new ResourceModel("command.add.widget"));
+				modal.setContent(new AddRuleDialog(modal.getContentId()) {
+
+					@Override
+					protected void onRuleEntered(AjaxRequestTarget target,
+							ResourceGeneric resource, String specific) {
+						String rule;
+						if(resource==null) rule=specific;
+						else {
+							rule = resource.getLegacyName();
+							if(specific!=null) rule+="."+specific;
+						}
+						ODocument doc = ORoleSecurityWidget.this.getModelObject();
+						Map<String, Byte> rules = doc.field("rules");
+						if(rules==null) {
+							rules = new HashMap<String, Byte>();
+							doc.field("rules", rules);
+						}
+						rules.put(rule, Byte.valueOf((byte)0));
+						doc.save();
+						modal.close(target);
+						target.add(ORoleSecurityWidget.this);
+					}
+					
+				});
+				modal.setAutoSize(true);
+				modal.setMinimalWidth(300);
+			}
+		});
 		sTable.addCommand(new AbstractDeleteCommand<String>(sTable) {
 
 			@Override
