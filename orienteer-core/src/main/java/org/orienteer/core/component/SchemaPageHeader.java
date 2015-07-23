98c321da60a2eaf545442dcb91d69bf936159255
evilsyncope <anton.v.maximov@gmail.com>
2015-07-24 04:48:48 +0600

    Navigation path for OClasses doesn't work properly. Fixes #248.

--- orienteer-core/src/main/java/org/orienteer/core/component/SchemaPageHeader.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/SchemaPageHeader.java
@@ -1,22 +1,23 @@
 package org.orienteer.core.component;
 
-import java.util.Objects;
-
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
 import org.orienteer.core.component.property.AbstractLinkViewPanel;
+import org.orienteer.core.component.property.OClassViewPanel;
 import org.orienteer.core.web.schema.SchemaPage;
+import ru.ydn.wicket.wicketorientdb.model.OClassModel;
 
-import com.orientechnologies.orient.core.index.OIndex;
-import com.orientechnologies.orient.core.metadata.schema.OClass;
-import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Page header for schema specific entities: {@link OClass}, {@link OProperty}, {@link OIndex}
@@ -25,7 +26,7 @@ public class SchemaPageHeader extends Panel {
 	private RepeatingView childRepeatingView;
 	private String lastComponentId;
 
-	public SchemaPageHeader(String id) {
+	public SchemaPageHeader(String id, IModel<OClass> oClassModel) {
 		super(id);
 		childRepeatingView = new RepeatingView("child");
 		add(childRepeatingView);
@@ -34,11 +35,19 @@ public class SchemaPageHeader extends Panel {
 			@Override
 			protected AbstractLink newLink(String id) {
 				return new BookmarkablePageLink<Object>(id, SchemaPage.class)
-									.setBody(new ResourceModel("menu.list.class"));
+						.setBody(new ResourceModel("menu.list.class"));
 			}
 		});
+
+		OClass currentClass = oClassModel.getObject();
+		List<OClass> superClasses;
+		if (currentClass != null) {
+			while ((superClasses = currentClass.getSuperClasses()) != null && !superClasses.isEmpty()) {
+				currentClass = superClasses.get(0);
+				addChild(new OClassViewPanel(newChildId(), new OClassModel(currentClass)));
+			}
+		}
 	}
-	
 	
 	public SchemaPageHeader addChild(Component component)
 	{
