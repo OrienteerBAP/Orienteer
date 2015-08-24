536211c83b091087c489305d240555df35742d91
evilsyncope <anton.v.maximov@gmail.com>
2015-08-24 09:56:15 +0600

    Widget to show subclasses for a class, fixes #270.

--- orienteer-core/src/main/java/org/orienteer/core/component/command/CreateOClassCommand.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/command/CreateOClassCommand.java
@@ -1,17 +1,14 @@
 package org.orienteer.core.component.command;
 
+import com.orientechnologies.orient.core.metadata.schema.OClass;
 import org.orienteer.core.component.property.DisplayMode;
-import org.orienteer.core.component.table.DataTableCommandsToolbar;
 import org.orienteer.core.component.table.OrienteerDataTable;
 import org.orienteer.core.web.schema.OClassPage;
-
 import ru.ydn.wicket.wicketorientdb.model.OClassModel;
 import ru.ydn.wicket.wicketorientdb.proto.OClassPrototyper;
 import ru.ydn.wicket.wicketorientdb.security.OSecurityHelper;
 import ru.ydn.wicket.wicketorientdb.security.OrientPermission;
 import ru.ydn.wicket.wicketorientdb.security.RequiredOrientResource;
-
-import com.orientechnologies.orient.core.metadata.schema.OClass;
 
 /**
  * {@link Command} to create {@link OClass}
@@ -19,15 +16,26 @@ import com.orientechnologies.orient.core.metadata.schema.OClass;
 @RequiredOrientResource(value = OSecurityHelper.SCHEMA, permissions=OrientPermission.CREATE)
 public class CreateOClassCommand extends AbstractCreateCommand<OClass>
 {
+	private OClass superClass;
 
 	public CreateOClassCommand(OrienteerDataTable<OClass, ?> table)
 	{
 		super(table);
 	}
 
+	public CreateOClassCommand(OrienteerDataTable<OClass, ?> table, OClass superClass) {
+		super(table);
+		this.superClass = superClass;
+	}
+
 	@Override
 	public void onClick() {
-		setResponsePage(new OClassPage(new OClassModel(OClassPrototyper.newPrototype())).setModeObject(DisplayMode.EDIT));
+		OClass oClass = OClassPrototyper.newPrototype();
+		if (superClass != null) {
+			oClass.addSuperClass(superClass);
+		}
+
+		setResponsePage(new OClassPage(new OClassModel(oClass)).setModeObject(DisplayMode.EDIT));
 	}
 
 }
--- orienteer-core/src/main/java/org/orienteer/core/component/widget/schema/OClassesWidget.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/widget/schema/OClassesWidget.java
@@ -1,92 +1,32 @@
 package org.orienteer.core.component.widget.schema;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.wicket.event.IEvent;
-import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
-import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.link.AbstractLink;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.orienteer.core.component.BootstrapType;
-import org.orienteer.core.component.FAIcon;
-import org.orienteer.core.component.FAIconType;
-import org.orienteer.core.component.OClassPageLink;
-import org.orienteer.core.component.command.BookmarkablePageLinkCommand;
-import org.orienteer.core.component.command.Command;
-import org.orienteer.core.component.command.CreateOClassCommand;
-import org.orienteer.core.component.command.DeleteOClassCommand;
-import org.orienteer.core.component.command.EditSchemaCommand;
-import org.orienteer.core.component.command.ExportOSchemaCommand;
-import org.orienteer.core.component.command.ImportOSchemaCommand;
-import org.orienteer.core.component.command.ReloadOMetadataCommand;
-import org.orienteer.core.component.command.SaveSchemaCommand;
-import org.orienteer.core.component.command.ViewUMLCommand;
-import org.orienteer.core.component.property.DisplayMode;
-import org.orienteer.core.component.table.CheckBoxColumn;
-import org.orienteer.core.component.table.OClassColumn;
-import org.orienteer.core.component.table.OClassMetaColumn;
-import org.orienteer.core.component.table.OrienteerDataTable;
-import org.orienteer.core.event.ActionPerformedEvent;
-import org.orienteer.core.web.BrowseOClassPage;
-import org.orienteer.core.widget.AbstractWidget;
-import org.orienteer.core.widget.Widget;
-
-import ru.ydn.wicket.wicketorientdb.model.OClassesDataProvider;
-import ru.ydn.wicket.wicketorientdb.proto.OClassPrototyper;
-import ru.ydn.wicket.wicketorientdb.utils.OClassClassNameConverter;
-
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import org.orienteer.core.component.FAIcon;
+import org.orienteer.core.component.FAIconType;
+import org.orienteer.core.component.command.*;
+import org.orienteer.core.component.property.DisplayMode;
+import org.orienteer.core.component.table.OrienteerDataTable;
+import org.orienteer.core.component.widget.AbstractOClassesListWidget;
+import org.orienteer.core.widget.Widget;
+import ru.ydn.wicket.wicketorientdb.model.AbstractJavaSortableDataProvider;
+import ru.ydn.wicket.wicketorientdb.model.OClassesDataProvider;
 
 /**
  * Widget to list all classes in the schema
  */
 @Widget(domain="schema", tab="classes", id="list-oclasses", autoEnable=true)
-public class OClassesWidget extends AbstractWidget<Void> {
+public class OClassesWidget extends AbstractOClassesListWidget<Void> {
 
-	
-	
 	public OClassesWidget(String id, IModel<Void> model,
 			IModel<ODocument> widgetDocumentModel) {
 		super(id, model, widgetDocumentModel);
-		
-		Form<?> form = new Form<Object>("form");
-		IModel<DisplayMode> modeModel = DisplayMode.VIEW.asModel();
-		List<IColumn<OClass, String>> columns = new ArrayList<IColumn<OClass,String>>();
-		columns.add(new CheckBoxColumn<OClass, String, String>(OClassClassNameConverter.INSTANCE));
-		columns.add(new OClassColumn(OClassPrototyper.NAME, modeModel));
-		columns.add(new OClassMetaColumn(OClassPrototyper.SUPER_CLASSES, modeModel));
-		columns.add(new OClassMetaColumn(OClassPrototyper.ABSTRACT, modeModel));
-		columns.add(new OClassMetaColumn(OClassPrototyper.STRICT_MODE, modeModel));
-		columns.add(new PropertyColumn<OClass, String>(new ResourceModel("class.count"), "count", "count"));
-		columns.add(new AbstractColumn<OClass, String>(new ResourceModel("class.browse")) {
+	}
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void populateItem(Item<ICellPopulator<OClass>> cellItem,
-					String componentId, final IModel<OClass> rowModel) {
-				cellItem.add(new BookmarkablePageLinkCommand<OClass>(componentId, "class.browse", BrowseOClassPage.class) {
-					public PageParameters getPageParameters() {
-						return BrowseOClassPage.preparePageParameters(rowModel.getObject(), DisplayMode.VIEW);
-					};
-				}.setIcon(FAIconType.angle_double_down).setBootstrapType(BootstrapType.INFO));
-			}
-		});
-		OClassesDataProvider provider = new OClassesDataProvider();
-		provider.setSort("name", SortOrder.ASCENDING);
-		OrienteerDataTable<OClass, String> table = new OrienteerDataTable<OClass, String>("table", columns, provider ,20);
+	@Override
+	protected void addTableCommands(OrienteerDataTable<OClass, String> table, IModel<DisplayMode> modeModel) {
 		table.addCommand(new CreateOClassCommand(table));
 		table.addCommand(new EditSchemaCommand<OClass>(table, modeModel));
 		table.addCommand(new SaveSchemaCommand<OClass>(table, modeModel));
@@ -95,8 +35,11 @@ public class OClassesWidget extends AbstractWidget<Void> {
 		table.addCommand(new ExportOSchemaCommand(table));
 		table.addCommand(new ImportOSchemaCommand(table));
 		table.addCommand(new ViewUMLCommand(table));
-		form.add(table);
-		add(form);
+	}
+
+	@Override
+	protected AbstractJavaSortableDataProvider getOClassesDataProvider() {
+		return new OClassesDataProvider();
 	}
 
 	@Override
