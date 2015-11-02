f95c5ac1053b8ddf07df03833b172db6762894db
Ilia Naryzhny <phantom@ydn.ru>
2015-11-01 17:57:33 -0800

    Update to new design

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
