9d2c5a90ebaefad4061ccbc41f2d71c0e0296ef1
Ilia Naryzhny <phantom@ydn.ru>
2015-12-13 23:22:30 -0800

    Get markup from page document

--- orienteer-pages/src/main/java/org/orienteer/pages/PagesWebPage.java
+++ orienteer-pages/src/main/java/org/orienteer/pages/PagesWebPage.java
@@ -1,10 +1,16 @@
 package org.orienteer.pages;
 
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.markup.IMarkupCacheKeyProvider;
+import org.apache.wicket.markup.IMarkupResourceStreamProvider;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.resource.StringResourceStream;
+import org.orienteer.pages.module.PagesModule;
 
 import ru.ydn.wicket.wicketorientdb.model.ODocumentModel;
 import ru.ydn.wicket.wicketorientdb.model.ODocumentPropertyModel;
@@ -15,21 +21,31 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 /**
  * Orienteer Pages {@link WebPage}
  */
-public class PagesWebPage extends WebPage {
+public class PagesWebPage extends WebPage implements IMarkupResourceStreamProvider, IMarkupCacheKeyProvider {
 	
-	private final IModel<ODocument> pageDocumentModel;
+	private final ODocumentModel pageDocumentModel;
 
 	public PagesWebPage(PageParameters parameters) {
 		super(parameters);
 		String orid = parameters.get(OPageParametersEncoder.PAGE_IDENTITY).toString();
 		pageDocumentModel = new ODocumentModel(new ORecordId(orid));
-		add(new Label("content", new ODocumentPropertyModel<String>(pageDocumentModel, "content")));
 	}
 	
 	@Override
 	public void detachModels() {
 		super.detachModels();
 		pageDocumentModel.detach();
+	}
+
+	@Override
+	public String getCacheKey(MarkupContainer container, Class<?> containerClass) {
+		return "OPage-"+pageDocumentModel.getIdentity().toString();
+	}
+
+	@Override
+	public IResourceStream getMarkupResourceStream(MarkupContainer container,
+			Class<?> containerClass) {
+		return new StringResourceStream((String) pageDocumentModel.getObject().field(PagesModule.OPROPERTY_CONTENT), "text/html");
 	}
 	
 	
