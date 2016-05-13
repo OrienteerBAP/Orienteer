e16110acdf9588908523292e435466a34a5c24f5
Ilia Naryzhny <phantom@ydn.ru>
2016-05-12 22:57:04 -0700

    Makeing dates and date-times display shorter

--- orienteer-core/src/main/java/org/orienteer/core/OrienteerWebApplication.java
+++ orienteer-core/src/main/java/org/orienteer/core/OrienteerWebApplication.java
@@ -67,8 +67,8 @@ public class OrienteerWebApplication extends OrientDbWebApplication
 {
 	private static final Logger LOG = LoggerFactory.getLogger(OrienteerWebApplication.class);
 	
-	public static final DateConverter DATE_CONVERTER = new StyleDateConverter("L-", true);
-	public static final DateConverter DATE_TIME_CONVERTER = new StyleDateConverter("LL", true);
+	public static final DateConverter DATE_CONVERTER = new StyleDateConverter("M-", true);
+	public static final DateConverter DATE_TIME_CONVERTER = new StyleDateConverter("MM", true);
 	
 	private LinkedHashMap<String, IOrienteerModule> registeredModules = new LinkedHashMap<String, IOrienteerModule>();
 	private boolean registeredModulesSorted = false;
