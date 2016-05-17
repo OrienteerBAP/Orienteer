67245a81baf873e75dbf4404b490484ca3dc08c2
Ilia Naryzhny <phantom@ydn.ru>
2016-05-16 22:08:28 -0700

    Make values for pivoting more user-friendly

--- orienteer-pivottable/src/main/java/org/orienteer/pivottable/component/widget/BrowsePivotTableWidget.java
+++ orienteer-pivottable/src/main/java/org/orienteer/pivottable/component/widget/BrowsePivotTableWidget.java
@@ -2,16 +2,22 @@ package org.orienteer.pivottable.component.widget;
 
 import static org.orienteer.core.module.OWidgetsModule.OPROPERTY_HIDDEN;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.util.encoding.UrlEncoder;
 import org.orienteer.core.behavior.UpdateOnDashboardDisplayModeChangeBehavior;
 import org.orienteer.core.component.FAIcon;
 import org.orienteer.core.component.FAIconType;
 import org.orienteer.core.component.property.DisplayMode;
+import org.orienteer.core.service.IOClassIntrospector;
 import org.orienteer.core.widget.AbstractWidget;
 import org.orienteer.core.widget.Widget;
 import org.orienteer.core.widget.support.jquery.JQueryDashboardSupport;
@@ -19,7 +25,10 @@ import org.orienteer.pivottable.PivotTableModule;
 import org.orienteer.pivottable.component.PivotPanel;
 
 import com.google.common.base.Objects;
+import com.google.inject.Inject;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 
 /**
@@ -30,13 +39,57 @@ public class BrowsePivotTableWidget extends AbstractWidget<OClass> {
 
 	private String config;
 	
+	@Inject
+	private IOClassIntrospector oClassIntrospector;
+	
 	public BrowsePivotTableWidget(String id, IModel<OClass> model,
 			IModel<ODocument> widgetDocumentModel) {
 		super(id, model, widgetDocumentModel);
-		add(new PivotPanel("pivot", new StringResourceModel("widget.pivottable.urlpattern.oclass", model),
+		add(new PivotPanel("pivot", new PropertyModel<String>(this, "url"),
 									new PropertyModel<DisplayMode>(this, "displayMode"),
 									new PropertyModel<String>(this, "config")));
 		add(UpdateOnDashboardDisplayModeChangeBehavior.INSTANCE);
+	}
+	
+	public String getUrl() {
+		String sql = getSql();
+		return "/orientdb/query/db/sql/"+
+					UrlEncoder.PATH_INSTANCE.encode(sql, "UTF-8")+
+				"/99999";
+	}
+	
+	public String getSql() {
+		String thisLang = getLocale().getLanguage();
+		String systemLang = Locale.getDefault().getLanguage();
+		OClass oClass = getModelObject();
+		StringBuilder sb = new StringBuilder();
+		Collection<OProperty> properties = oClass.properties();
+		for(OProperty property: properties) {
+			OType type = property.getType();
+			if(Comparable.class.isAssignableFrom(type.getDefaultJavaType())) {
+				sb.append(property.getName()).append(", ");
+			} else if(OType.LINK.equals(type)) {
+				OClass linkedClass = property.getLinkedClass();
+				OProperty nameProperty = oClassIntrospector.getNameProperty(linkedClass);
+				if(nameProperty!=null) {
+					OType linkedClassType = nameProperty.getType();
+					String map = property.getName()+'.'+nameProperty.getName();
+					if(Comparable.class.isAssignableFrom(linkedClassType.getDefaultJavaType())) {
+						sb.append(map).append(", ");
+					} else if (OType.EMBEDDEDMAP.equals(linkedClassType)) {
+						sb.append("coalesce(").append(map).append('[').append(thisLang).append("], ");
+						if(!thisLang.equals(systemLang)) {
+							sb.append(map).append('[').append(systemLang).append("], ");
+						}
+						sb.append("first(").append(map).append(")) as ").append(property.getName()).append(", ");
+					}
+				}
+			}
+		}
+		if(sb.length()>0) sb.setLength(sb.length()-2);
+		sb.insert(0, "SELECT ");
+		sb.append(" FROM ").append(oClass.getName());
+		return sb.toString();
 	}
 
 	@Override
