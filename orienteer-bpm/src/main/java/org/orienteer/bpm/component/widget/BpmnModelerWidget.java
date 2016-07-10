7811ebb81c50005bbd9b9d7f78f0657bf4aec987
Ilia Naryzhny <phantom@ydn.ru>
2016-07-10 15:30:27 -0700

    Avoid extra panel for BPMN modeler + some OOB buttons

--- orienteer-bpm/src/main/java/org/orienteer/bpm/component/widget/BpmnModelerWidget.java
+++ orienteer-bpm/src/main/java/org/orienteer/bpm/component/widget/BpmnModelerWidget.java
@@ -1,14 +1,24 @@
 package org.orienteer.bpm.component.widget;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.util.template.PackageTextTemplate;
+import org.apache.wicket.util.template.TextTemplate;
 import org.orienteer.bpm.camunda.handler.ProcessDefinitionEntityHandler;
 import org.orienteer.bpm.camunda.handler.ResourceEntityHandler;
 import org.orienteer.core.component.FAIcon;
 import org.orienteer.core.component.FAIconType;
+import org.orienteer.core.component.property.DisplayMode;
+import org.orienteer.core.util.CommonUtils;
 import org.orienteer.core.widget.AbstractModeAwareWidget;
 import org.orienteer.core.widget.AbstractWidget;
 import org.orienteer.core.widget.Widget;
@@ -16,23 +26,39 @@ import org.orienteer.core.widget.Widget;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
+import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
+import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 import ru.ydn.wicket.wicketorientdb.model.AbstractConverterModel;
 import ru.ydn.wicket.wicketorientdb.model.ODocumentPropertyModel;
 import ru.ydn.wicket.wicketorientdb.model.OQueryModel;
+import ru.ydn.wicket.wicketorientdb.security.OSecurityHelper;
+import ru.ydn.wicket.wicketorientdb.security.OrientPermission;
 
 /**
  * Widget for BPMN modeling
  */
 @Widget(id="bpmn-modeler", domain="document", selector=ProcessDefinitionEntityHandler.OCLASS_NAME, autoEnable=true)
-public class BpmnModelerWidget extends AbstractModeAwareWidget<ODocument> {
+public class BpmnModelerWidget extends AbstractWidget<ODocument> {
 	
-	private OQueryModel<ODocument> resourcesModel; 
+	private static final WebjarsCssResourceReference BPMN_DIAGRAM_CSS = new WebjarsCssResourceReference("bpmn-js/current/dist/assets/diagram-js.css");
+	private static final WebjarsCssResourceReference BPMN_EMBEDDED_CSS = new WebjarsCssResourceReference("bpmn-js/current/dist/assets/bpmn-font/css/bpmn-embedded.css");
+	private static final WebjarsCssResourceReference BPMN_CSS = new WebjarsCssResourceReference("bpmn-js/current/dist/assets/bpmn-font/css/bpmn.css");
+	private static final WebjarsJavaScriptResourceReference BPMN_VIEWER_JS = new WebjarsJavaScriptResourceReference("bpmn-js/current/dist/bpmn-viewer.js");
+	private static final WebjarsJavaScriptResourceReference BPMN_MODELER_JS = new WebjarsJavaScriptResourceReference("bpmn-js/current/dist/bpmn-modeler.js");
+	
+	private IModel<ODocument> resourceModel;
 	
 	public BpmnModelerWidget(String id, IModel<ODocument> model, IModel<ODocument> widgetDocumentModel) {
 		super(id, model, widgetDocumentModel);
-		add(new BpmnModelerPanel("modeler", new PropertyModel<String>(this, "bpmn"), getModeModel()));
-		resourcesModel = new OQueryModel<>("select from "+ResourceEntityHandler.OCLASS_NAME+" where name = :resourceName");
+		OQueryModel<ODocument> resourcesModel = new OQueryModel<>("select from "+ResourceEntityHandler.OCLASS_NAME+" where name = :resourceName");
 		resourcesModel.setParameter("resourceName", new ODocumentPropertyModel<String>(model, "resourceName"));
+		resourceModel = new AbstractConverterModel<List<ODocument>, ODocument>(resourcesModel) {
+
+			@Override
+			protected ODocument doForward(List<ODocument> a) {
+				return a==null || a.isEmpty() ? null: a.get(0);
+			}
+		};
 	}
 
 	@Override
@@ -45,24 +71,37 @@ public class BpmnModelerWidget extends AbstractModeAwareWidget<ODocument> {
 		return new ResourceModel("widget.bpmnmodeler");
 	}
 	
-	public String getBpmn() {
-		List<ODocument> resources = resourcesModel.getObject();
-		if(resources==null || resources.isEmpty()) return null;
-		else {
-			ODocument resource = resources.get(0);
-			byte[] bytes = resource.field("bytes", String.class);
-			return new String(bytes);
-		}
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(CssHeaderItem.forReference(BPMN_DIAGRAM_CSS));
+		response.render(CssHeaderItem.forReference(BPMN_EMBEDDED_CSS));
+//		response.render(CssHeaderItem.forReference(BPMN_CSS));
+//		if(DisplayMode.EDIT.equals(modeModel.getObject())) {
+			response.render(JavaScriptHeaderItem.forReference(BPMN_VIEWER_JS));
+			response.render(JavaScriptHeaderItem.forReference(BPMN_MODELER_JS));
+//		} else {
+//		}
+		ODocument resource = resourceModel.getObject();
+		TextTemplate template = new PackageTextTemplate(BpmnModelerWidget.class, "modeler.tmpl.js");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("componentId", getMarkupId());
+		params.put("canEdit", canEdit(getModelObject(), resource));
+		byte[] xmlBytes = resource!=null?(byte[])resource.field("bytes"):null;
+		String xml = xmlBytes!=null?new String(xmlBytes):null;
+		params.put("xml", CommonUtils.escapeAndWrapAsJavaScriptString(xml));
+		response.render(OnDomReadyHeaderItem.forScript(template.asString(params)));
 	}
 	
-	public void setBpmn(String bpmn) {
-		System.out.println("BPMN: "+bpmn);
+	protected boolean canEdit(ODocument processDefinition, ODocument resource) {
+		return OSecurityHelper.isAllowed(processDefinition, OrientPermission.UPDATE)
+				&& OSecurityHelper.isAllowed(resource, OrientPermission.UPDATE);
 	}
 	
 	@Override
 	public void detachModels() {
 		super.detachModels();
-		resourcesModel.detach();
+		resourceModel.detach();
 	}
 
 }
--- orienteer-bpm/src/main/resources/org/orienteer/bpm/component/widget/modeler.tmpl.js
+++ orienteer-bpm/src/main/resources/org/orienteer/bpm/component/widget/modeler.tmpl.js
@@ -1,15 +1,162 @@
 var BpmnViewer = window.BpmnJS.Viewer;
 var BpmnModeler = window.BpmnJS;
 
+
+var container = $('#${componentId} .modeler');
+
+var canvas = $('#${componentId} .canvas');
+
 //var viewer = new BpmnViewer({ container: '#${componentId}', height: 600 });
-var viewer = new BpmnModeler({ container: '#${componentId}'});
+var modeler = new BpmnModeler({ container: canvas});
 var xml = ${xml};
 
-viewer.importXML(xml, function(err) {
+var newDiagram = '<?xml version="1.0" encoding="UTF-8"?>'+
+'<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn">'+
+'  <bpmn2:process id="Process_1" isExecutable="false"><bpmn2:startEvent id="StartEvent_1"/></bpmn2:process>'+
+'  <bpmndi:BPMNDiagram id="BPMNDiagram_1">'+
+'    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">'+
+'      <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">'+
+'        <dc:Bounds height="36.0" width="36.0" x="412.0" y="240.0"/>'+
+'      </bpmndi:BPMNShape>'+
+'    </bpmndi:BPMNPlane>'+
+'  </bpmndi:BPMNDiagram>'+
+'</bpmn2:definitions>';
 
-  if (err) {
-    console.log('error rendering', err);
-  } else {
-    console.log('rendered');
+if(xml == null) xml = newDiagram;
+
+function openDiagram(xml) {
+
+  modeler.importXML(xml, function(err) {
+
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+
+
+  });
+}
+
+function saveSVG(done) {
+  modeler.saveSVG(done);
+}
+
+function saveDiagram(done) {
+
+  modeler.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+
+
+function registerFileDrop(container, callback) {
+
+  function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.dataTransfer.files;
+
+    var file = files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+
+      var xml = e.target.result;
+
+      callback(xml);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  container.get(0).addEventListener('dragover', handleDragOver, false);
+  container.get(0).addEventListener('drop', handleFileSelect, false);
+}
+
+if (!window.FileList || !window.FileReader) {
+  window.alert(
+    'Looks like you use an older browser that does not support drag and drop. ' +
+    'Try using Chrome, Firefox or the Internet Explorer > 10.');
+} else {
+  registerFileDrop(container, openDiagram);
+}
+
+
+var resetLink = $('#${componentId} .js-reset');
+var downloadLink = $('#${componentId} .js-download-diagram');
+var downloadSvgLink = $('#${componentId} .js-download-svg');
+
+$('.buttons a').click(function(e) {
+  if (!$(this).is('.active')) {
+    e.preventDefault();
+    e.stopPropagation();
   }
 });
+
+resetLink.click(function(e) {
+	openDiagram(newDiagram);
+	return false;
+});
+
+function setEncoded(link, name, data) {
+  var encodedData = encodeURIComponent(data);
+
+  if (data) {
+    link.addClass('active').attr({
+      'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+      'download': name
+    });
+  } else {
+    link.removeClass('active');
+  }
+}
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+var exportArtifacts = debounce(function() {
+
+saveSVG(function(err, svg) {
+  setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
+});
+
+saveDiagram(function(err, xml) {
+  setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+    });
+  }, 500);
+
+modeler.on('commandStack.changed', exportArtifacts);
+
+openDiagram(xml);
+exportArtifacts();
