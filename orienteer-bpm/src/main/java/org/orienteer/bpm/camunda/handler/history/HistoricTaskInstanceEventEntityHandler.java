208231b762700c1cf084d25b0bf89c5533c05334
Kirill Mukhov <dissdoc@gmail.com>
2016-08-23 22:01:33 +0300

    add select query

--- orienteer-bpm/src/main/java/org/orienteer/bpm/camunda/handler/history/HistoricTaskInstanceEventEntityHandler.java
+++ orienteer-bpm/src/main/java/org/orienteer/bpm/camunda/handler/history/HistoricTaskInstanceEventEntityHandler.java
@@ -3,12 +3,17 @@ package org.orienteer.bpm.camunda.handler.history;
 import com.google.common.base.Function;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import org.camunda.bpm.engine.impl.db.ListQueryParameterObject;
 import org.camunda.bpm.engine.impl.history.event.HistoricTaskInstanceEventEntity;
 import org.orienteer.bpm.camunda.OPersistenceSession;
 import org.orienteer.bpm.camunda.handler.IEntityHandler;
 import org.orienteer.bpm.camunda.handler.NonUniqIdConverter;
+import org.orienteer.bpm.camunda.handler.Statement;
 import org.orienteer.core.util.OSchemaHelper;
 import ru.ydn.wicket.wicketorientdb.utils.GetODocumentFieldValueFunction;
+
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -47,5 +52,12 @@ public class HistoricTaskInstanceEventEntityHandler extends HistoricScopeInstanc
     protected void initMapping(OPersistenceSession session) {
         super.initMapping(session);
         mappingConvertors.put("id", new NonUniqIdConverter("ti:"));
+    }
+
+    @Statement
+    public String selectHistoricTaskInstanceIdsByParameters(OPersistenceSession session, ListQueryParameterObject parameters) {
+        Map<String, String> params = (Map<String, String>) parameters.getParameter();
+        List<HistoricTaskInstanceEventEntity> list = query(session, params);
+        return list == null || list.size() < 1 ? null : list.get(0).getId();
     }
 }
