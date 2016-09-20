842fd04e89ab7fbb15e1fb1b3c444a500353d6f6
Sergey Arabskii <barm@mail.ru>
2016-09-20 14:16:54 +0300

    another ugly commit

--- orienteer-core/src/main/java/org/orienteer/core/component/widget/oclass/OClassHooksWidget.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/widget/oclass/OClassHooksWidget.java
@@ -3,81 +3,116 @@
  */
 package org.orienteer.core.component.widget.oclass;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.util.string.Strings;
+import org.orienteer.core.CustomAttributes;
 import org.orienteer.core.component.FAIcon;
 import org.orienteer.core.component.FAIconType;
 import org.orienteer.core.component.command.EditODocumentCommand;
 import org.orienteer.core.component.command.EditSchemaCommand;
 import org.orienteer.core.component.command.SaveODocumentCommand;
 import org.orienteer.core.component.command.SaveSchemaCommand;
+import org.orienteer.core.component.meta.AbstractModeMetaPanel;
 import org.orienteer.core.component.meta.OClassMetaPanel;
 import org.orienteer.core.component.meta.ODocumentMetaPanel;
 import org.orienteer.core.component.property.DisplayMode;
 import org.orienteer.core.component.structuretable.OrienteerStructureTable;
+import org.orienteer.core.component.structuretable.StructureTable;
+import org.orienteer.core.component.widget.AbstractSchemaCustomPropertiesWidget;
 import org.orienteer.core.component.widget.document.ODocumentNonRegisteredPropertiesWidget;
 import org.orienteer.core.service.IOClassIntrospector;
 import org.orienteer.core.widget.AbstractModeAwareWidget;
 import org.orienteer.core.widget.Widget;
 
 import com.google.inject.Inject;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.function.OFunction;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OClassImpl;
 import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import com.orientechnologies.orient.core.type.ODocumentWrapper;
 
+import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
 import ru.ydn.wicket.wicketorientdb.model.ListOPropertiesModel;
+import ru.ydn.wicket.wicketorientdb.model.ODocumentPropertyModel;
+import ru.ydn.wicket.wicketorientdb.model.OQueryModel;
+import ru.ydn.wicket.wicketorientdb.proto.OClassPrototyper;
 
 /**
  * @author Asm
  * Widget for class-linked hooks
  */
-/*
- * приделать кнопочку редактирования
- * 
- * сделать разделение на редактирование и просмотр
- * 
- * сделать обработку заглушечных полей(эт поначалу они заглушечные ж ,а потооом...)
- * 
- * приделать список ListOFunctionsModel или как -то так ,наподобие ListOPropertiesModel
- * 
- * по-хорошему ,надо бы прихерачить отдельный селектор для функций.
- * 
- * */
 @Widget(domain="class", tab="configuration",selector="OTriggered", id = OClassHooksWidget.WIDGET_TYPE_ID, order=20, autoEnable=true)
 public class OClassHooksWidget extends AbstractModeAwareWidget<OClass> {
 
 	public static final String WIDGET_TYPE_ID = "classHooks";
 	
-    private OrienteerStructureTable<OClass, String> propertiesStructureTable;
+    private ListView<String> propertiesListView;
+    //private OrienteerStructureTable<OClass, String> propertiesStructureTable;
     private SaveSchemaCommand saveODocumentCommand;
     
     @Inject
     private IOClassIntrospector oClassIntrospector;
+	private OrienteerStructureTable<ArrayList<String>, String> structureTable;
 
+	public static final List<String> EVENTS_LIST = new ArrayList<String>();
+	static
+	{
+		//EVENTS_LIST.add("");
+		EVENTS_LIST.add("onBeforeCreate");
+		EVENTS_LIST.add("onAfterCreate");
+		EVENTS_LIST.add("onBeforeRead");
+		EVENTS_LIST.add("onAfterRead");
+		EVENTS_LIST.add("onBeforeUpdate");
+		EVENTS_LIST.add("onAfterUpdate");
+		EVENTS_LIST.add("onBeforeDelete");
+		EVENTS_LIST.add("onAfterDelete");
+	}		
+	
+	public class Events implements Serializable{
+		protected String onBeforeCreate;
+		protected String onAfterCreate;
+		protected String onBeforeRead;
+		protected String onAfterRead;
+		protected String onBeforeUpdate;
+		protected String onAfterUpdate;
+		protected String onBeforeDelete;
+		protected String onAfterDelete;
+		
+	}
 	
 //getModeModel
 	public OClassHooksWidget(String id, final IModel<OClass> model, IModel<ODocument> widgetDocumentModel) {
 		super(id, model, widgetDocumentModel);
-		
+		/*
 		final List<String> events = new ArrayList<String>();
 		events.add("onBeforeCreate");
 		events.add("onAfterCreate");
@@ -87,7 +122,15 @@ public class OClassHooksWidget extends AbstractModeAwareWidget<OClass> {
 		events.add("onAfterUpdate");
 		events.add("onBeforeDelete");
 		events.add("onAfterDelete");
+		*/
+		/*
+		HashMap<String, String> eventsMap = new HashMap<String, String>();
+		for(int i=0; i <events.size();i++){
+			eventsMap.put(events.get(i), "");
+		}
+		*/
 
+		/*
 		IModel<List<OProperty>> propertiesModel = new LoadableDetachableModel<List<OProperty>>() {
 			@Override
 			protected List<OProperty> load() {
@@ -108,8 +151,141 @@ public class OClassHooksWidget extends AbstractModeAwareWidget<OClass> {
 				return ret;
 			}
 		};
+*/
+		//final List<String> events2db = new ArrayList<String>();
+		//for (String val : EVENTS_LIST) {
+		//	events2db.add("customFields."+val+" as "+val);
+		//}
 
-		Form form = new Form("form");
+		//final OQueryModel<ODocument> q= new OQueryModel<ODocument>("select "+Strings.join(",", events2db)+" from (SELECT expand(classes) from metadata:schema) where name=\""+model.getObject().getName()+"\"");
+		
+		final ListView<String> eventsView = new ListView<String>("events", EVENTS_LIST) {
+			@Override
+			protected void populateItem(ListItem<String> item) {
+				item.add(new Label("description", item.getModel()));
+				//item.add(new Label("description2", Model.of(Strings.toString(q.getObject().get(0).field(item.getModelObject().toString())))));
+				//model.getObject().getCustom(item.getModelObject())
+				item.add(new DropDownChoice<String>(
+						"input",
+						Model.of(model.getObject().getCustom(item.getModelObject())),
+						 new ArrayList<String>(getDatabase().getMetadata().getFunctionLibrary().getFunctionNames())
+				).setNullValid(true));
+			}
+		};
+		Form form = new Form("form"){
+			@Override
+			protected void onSubmit() {
+				//String setq = "ALTER CLASS \""+model.getObject().getName()+"\" CUSTOM ";
+				List<String> retarr = new ArrayList<String>();
+				
+				for (Component val : eventsView) {
+					String eventVal = ((ListItem<String>)val).get("input").getDefaultModelObjectAsString();
+					String eventName = ((ListItem<String>)val).get("description").getDefaultModelObjectAsString();
+					//retarr.add(((ListItem<String>)val).get("description").getDefaultModelObjectAsString()+"=\""+((ListItem<String>)val).get("input").getDefaultModelObjectAsString()+"\"");
+			    	ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+			    	db.commit();
+			    	model.getObject().setCustom(eventName, eventVal.isEmpty()?null:eventVal);
+			    	
+
+//					String retq;
+					
+					
+					//if (Strings.isEmpty(retVal)){
+					//	retq="ALTER CLASS "+model.getObject().getName()+" CUSTOM "+((ListItem<String>)val).get("description").getDefaultModelObjectAsString()+"=\"\"";
+						
+					//}else{
+					//	retq="ALTER CLASS "+model.getObject().getName()+" CUSTOM "+((ListItem<String>)val).get("description").getDefaultModelObjectAsString()+"="+((ListItem<String>)val).get("input").getDefaultModelObjectAsString()+"";
+					//}
+					
+			    	//OCommandSQL query = new OCommandSQL(retq);
+			    	//db.command(query);
+			    	//db.commit();
+					//debug(retq);
+					//events2db.add("customFields."+val+" as "+val);
+				}
+				//new OQueryModel<String>(retq);
+//				retq.
+				/*
+				for(Iterator<String> i = someList.iterator(); i.hasNext(); ) {
+				    String item = i.next();
+				    System.out.println(item);
+				}
+				*/
+				super.onSubmit();
+			}
+		};
+		
+		form.add(eventsView);
+		/* //совершенно не понимаю, что ему тут надо 
+		Events ev = new Events(); 
+		form.add(new OrienteerStructureTable<Events,String>("events",Model.of(ev), EVENTS_LIST){
+			@Override
+			protected Component getValueComponent(String id, IModel<String> rowModel) {
+				return new AbstractModeMetaPanel<Events, DisplayMode, String, String>(id, getModeModel(), OClassHooksWidget.this.getModel(), rowModel) {
+
+					@Override
+					protected Component resolveComponent(String id,	DisplayMode mode, String critery) {
+						return null;
+						
+						//if(DisplayMode.EDIT.equals(mode)) {
+						//	return new TextArea<String>(id, getValueModel());
+						//} else {
+						//	return new MultiLineLabel(id, getValueModel());
+						//}
+						
+					}
+
+					@Override
+					protected IModel<String> newLabelModel() {
+						return null;//getPropertyModel();
+					}
+
+					@Override
+					protected IModel<String> resolveValueModel() {
+						return null;//createCustomModel(getEntityModel(), getPropertyModel());
+					}
+				};
+			}
+		}
+		);
+		*/
+		/*
+		structureTable = new OrienteerStructureTable<ArrayList<String>, String>("table", Model.of(events), new PropertyModel<List<String>>(this, "custom")) {
+
+			@Override
+			protected Component getValueComponent(String id,
+					IModel<String> rowModel) {
+				return new AbstractModeMetaPanel<T, DisplayMode, String, String>(id, getModeModel(), AbstractSchemaCustomPropertiesWidget.this.getModel(), rowModel) {
+
+					@Override
+					protected Component resolveComponent(String id,
+							DisplayMode mode, String critery) {
+						if(DisplayMode.EDIT.equals(mode)) {
+							return new TextArea<String>(id, getValueModel());
+						} else {
+							return new MultiLineLabel(id, getValueModel());
+						}
+					}
+
+					@Override
+					protected IModel<String> newLabelModel() {
+						return getPropertyModel();
+					}
+
+					@Override
+					protected IModel<String> resolveValueModel() {
+						return createCustomModel(getEntityModel(), getPropertyModel());
+					}
+				};
+				
+			}
+		};
+		*/
+		
+		//for (String val : events) {
+		//	form.add(new Label(val, new OQueryModel<ODocument>("select "+Strings.join(",", events2db)+" from (SELECT expand(classes) from metadata:schema) where name=\""+model.getObject().getName()+"\"")));
+		//}
+	
 		/*
 		form.add(new DropDownChoice<String>(
 				"select",
@@ -118,39 +294,37 @@ public class OClassHooksWidget extends AbstractModeAwareWidget<OClass> {
 		));
 */
 		/*
-		propertiesStructureTable = new OrienteerStructureTable<OClassImpl, OProperty>("properties", getModel(), propertiesModel){
-
+		form.add( propertiesListView = new ListView<String>("properties", events) {
 			@Override
-			protected Component getValueComponent(String id,IModel<OProperty> rowModel) {
-				return new Label(id,rowModel.getObject().getName());
-				//return new TextField<String>(id, Model.of(""), String.class);
-				//return new TextField<OProperty>(id, rowModel, OProperty.class);
-				//return new OClassMetaPanel<Object>(id, getModeModel(), OClassHooksWidget.this.getModel(), Model.of(rowModel.getObject().getName()));
-				//return new OClassMetaPanel<Object>(id, getModeModel(), OClassHooksWidget.this.getModel(), rowModel);
+			protected void populateItem(ListItem<String> item) {
+				item.add(new Label("description", item.getModel()));
+				item.add(new TextField<String>("input", item.getModel(), String.class));
 			}
-		};
+		});
 		*/
-		
+		/*
 		propertiesStructureTable  = new OrienteerStructureTable<OClass,String>("properties", getModel(), events) {
 			@Override
 			protected Component getValueComponent(String id, final IModel<String> rowModel) {
+				return new TextField<String>(id, rowModel, String.class);
 				//return new Label(id,rowModel);
-				return new OClassMetaPanel<Object>(id, getModeModel(), OClassHooksWidget.this.getModel(), rowModel);
+				//return new OClassMetaPanel<Object>(id, getModeModel(), OClassHooksWidget.this.getModel(), rowModel);
 			}
 			
 		};
-		
-		form.add(propertiesStructureTable);
+		*/
+		//form.add(propertiesStructureTable);
+		//getDatabase().getMetadata().getSchema().
 		add(form);
-		add(new Label("text", "^^^^^^"+(String)getDatabase().getMetadata().getFunctionLibrary().getFunction("func1").execute()));
+		//add(new Label("text", "^^^^^^"+(String)getDatabase().getMetadata().getFunctionLibrary().getFunction("func1").execute()));
 	}
 	
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		propertiesStructureTable.addCommand(new EditSchemaCommand(propertiesStructureTable, getModeModel()));
+		//propertiesStructureTable.addCommand(new EditSchemaCommand(propertiesStructureTable, getModeModel()));
 		
-		propertiesStructureTable.addCommand(saveODocumentCommand = new SaveSchemaCommand(propertiesStructureTable, getModeModel()));
+		//propertiesStructureTable.addCommand(saveODocumentCommand = new AbstractSaveCommand(propertiesStructureTable, getModeModel()));
 	}
 	
 	@Override
@@ -165,33 +339,6 @@ public class OClassHooksWidget extends AbstractModeAwareWidget<OClass> {
 			}
 
 		//}
-	}
-	
-	protected Component getDefaultView(){
-		return getDisplayView();
-	}
-	
-	protected Component getEditView(){
-
-		Form form = new Form("form");
-		form.add(new DropDownChoice<String>(
-				"select",
-				//Model.of(""),
-				 new ArrayList<String>(getDatabase().getMetadata().getFunctionLibrary().getFunctionNames())
-		));
-
-		form.add(propertiesStructureTable);
-
-		Fragment fragment = new  Fragment ("body", "editView", this);
-		fragment.add(form);
-		return fragment;
-	}
-	
-	protected Component getDisplayView(){
-		
-		Fragment fragment = new  Fragment ("body", "displayView", this);
-		fragment.add(new Label("text", "^^^^^^"+(String)getDatabase().getMetadata().getFunctionLibrary().getFunction("func1").execute()));
-		return fragment;
 	}
 	
 
