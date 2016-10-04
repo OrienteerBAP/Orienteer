ef1dd067d0542585e66757a7ed852823694c5207
Sergey Arabskii <barm@mail.ru>
2016-10-04 10:42:57 +0300

    The logic was changed. Now we make new property on his page

--- orienteer-core/src/main/java/org/orienteer/core/component/property/UnregistredPropertyEditPanel.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/property/UnregistredPropertyEditPanel.java
@@ -6,19 +6,11 @@ package org.orienteer.core.component.property;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.ResourceModel;
-import org.orienteer.core.behavior.UpdateOnActionPerformedEventBehavior;
+import org.orienteer.core.web.schema.OPropertyPage;
 
-import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.schema.OProperty;
-import com.sun.mail.handlers.image_gif;
-
-import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
-import ru.ydn.wicket.wicketorientdb.proto.IPrototype;
 
 /**
  * To making fields in classes by fields in documents 
@@ -34,14 +26,7 @@ public class UnregistredPropertyEditPanel extends GenericPanel<OProperty> {
 		return new AjaxLink("realize") {
 			@Override
 			public void onClick(AjaxRequestTarget target) {
-				ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
-				boolean isActiveTransaction = db.getTransaction().isActive();
-				if(isActiveTransaction) db.commit();
-				((IPrototype<?>)UnregistredPropertyEditPanel.this.getModel().getObject()).realizePrototype();
-				setVisibilityAllowed(false);
-				target.add(this);
-				String message = getLocalizer().getString("widget.document.unregistered.properties.success", this).replace("\"", "\\\"");
-				info(message);
+				setResponsePage(new OPropertyPage(UnregistredPropertyEditPanel.this.getModel()).setModeObject(DisplayMode.EDIT));
 			}
 		};
 	}
@@ -53,6 +38,5 @@ public class UnregistredPropertyEditPanel extends GenericPanel<OProperty> {
 	public String getPropertyComponentId() {
 		return "propertyComponent";
 	}
-	
 	
 }
