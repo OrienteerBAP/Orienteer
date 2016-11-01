e496cc3c6b921d500e11c80ec4856f1a5c75e13c
Sergey Arabskii <barm@mail.ru>
2016-11-01 12:44:31 +0300

    added base orientdb sql support for wicket-console

---
+++ orienteer-devutils/src/main/java/org/orienteer/devutils/component/ODBScriptEngineInterlayerResult.java
@@ -0,0 +1,60 @@
+package org.orienteer.devutils.component;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+
+import ru.ydn.wicket.wicketconsole.IScriptEngineInterlayerResult;
+import ru.ydn.wicket.wicketconsole.IScriptEngineInterlayerResultRenderer;
+
+public class ODBScriptEngineInterlayerResult implements IScriptEngineInterlayerResult {
+
+	private String error; 
+	private String out;
+	private Object returnedObject;
+	private transient IScriptEngineInterlayerResultRenderer renderer;
+	
+	public ODBScriptEngineInterlayerResult() {
+		renderer = new ODBScriptEngineInterlayerResultRenderer(this);
+	}
+
+	protected void setOut(String out) {
+		this.out = out;
+	}
+
+	@Override
+	public String getOut() {
+		return out;
+	}
+
+	@Override
+	public String getError() {
+		return error;
+	}
+	
+	protected void setError(String error) {
+		this.error = error;
+	}
+
+	@Override
+	public Object getReturnedObject() {
+		return returnedObject;
+	}
+	
+	protected void setReturnedObject(Object returnedObject) {
+		this.returnedObject = returnedObject;
+	}
+
+	@Override
+	public void onUpdate() {
+		
+	}
+
+	@Override
+	public IScriptEngineInterlayerResultRenderer getRenderer() {
+		return renderer;
+	}
+
+	@Override
+	public void setRenderer(IScriptEngineInterlayerResultRenderer renderer) {
+		this.renderer = renderer;
+	}
+}
