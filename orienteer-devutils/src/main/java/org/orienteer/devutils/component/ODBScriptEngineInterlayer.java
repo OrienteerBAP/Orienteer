a1f41f7ed91128e588479e59cc749c8ece9afaa9
Sergey Arabskii <barm@mail.ru>
2016-11-04 08:33:21 +0300

    Devutils sql support  and polish (#423)
    * added base orientdb sql support for wicket-console
    
    * added models
    
    * added renderers manager support and some comments

---
+++ orienteer-devutils/src/main/java/org/orienteer/devutils/component/ODBScriptEngineInterlayer.java
@@ -0,0 +1,47 @@
+package org.orienteer.devutils.component;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+
+import ru.ydn.wicket.wicketconsole.IScriptEngineInterlayer;
+import ru.ydn.wicket.wicketconsole.IScriptEngineInterlayerResult;
+import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
+
+/**
+ * Orient DB script interlayer for  {@link ScriptEngineInterlayerManager}  
+ *
+ */
+
+public class ODBScriptEngineInterlayer implements IScriptEngineInterlayer{
+
+	String name;
+	
+	public ODBScriptEngineInterlayer() {
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public IScriptEngineInterlayerResult eval(String command) {
+		ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+		ODBScriptEngineInterlayerResult result = new ODBScriptEngineInterlayerResult();
+		db.commit();
+		OCommandSQL comm = new OCommandSQL(command);
+		try{
+			result.setReturnedObject(db.command(comm).execute());
+			result.onUpdate();
+		}catch(Exception e){
+			result.setError(e.getMessage());
+		}
+		return result;
+	}
+	
+}
