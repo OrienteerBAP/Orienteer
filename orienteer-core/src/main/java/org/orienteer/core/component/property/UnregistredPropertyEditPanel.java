d7b714ea8e355943d8f57478d76932a4cd4aa6c9
Ilia Naryzhny <phantom@ydn.ru>
2017-01-23 22:41:14 -0800

    Fix issue with absence of markup: related to #454

--- orienteer-core/src/main/java/org/orienteer/core/component/property/UnregistredPropertyEditPanel.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/property/UnregistredPropertyEditPanel.java
@@ -6,16 +6,22 @@ package org.orienteer.core.component.property;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.IMarkupFragment;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
+import org.orienteer.core.service.IMarkupProvider;
 import org.orienteer.core.web.schema.OPropertyPage;
 
+import com.google.inject.Inject;
 import com.orientechnologies.orient.core.metadata.schema.OProperty;
 
 /**
  * To making fields in classes by fields in documents 
  */
 public class UnregistredPropertyEditPanel extends GenericPanel<OProperty> {
+	
+	@Inject
+	private IMarkupProvider markupProvider;
 
 	public UnregistredPropertyEditPanel(String id, IModel<OProperty> model) {
 		super(id, model);
@@ -29,6 +35,15 @@ public class UnregistredPropertyEditPanel extends GenericPanel<OProperty> {
 				setResponsePage(new OPropertyPage(UnregistredPropertyEditPanel.this.getModel()).setModeObject(DisplayMode.EDIT));
 			}
 		};
+	}
+	
+	@Override
+	public IMarkupFragment getMarkup(Component child) {
+		if(child!=null && child.getId().equals(getPropertyComponentId())) {
+			return markupProvider.provideMarkup(child);
+		} else {
+			return super.getMarkup(child);
+		}
 	}
 	
 	public void setPropertyComponent(Component propertyComponent){
