00bbd92e968cd20200fbc94894b6f5d3e913ec2b
IvanSCM <ivanscm@fromru.com>
2017-01-28 01:02:15 +0700

    Tags visualizer for link... data types. #428 (#457)
    * Tags visualizer for link... data types. #428
    
    * Fix. Move Provider to file, add java doc

---
+++ orienteer-core/src/main/java/org/orienteer/core/component/visualizer/TagsVisualizer.java
@@ -0,0 +1,42 @@
+package org.orienteer.core.component.visualizer;
+
+import java.util.Collection;
+
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import org.apache.wicket.Component;
+import org.apache.wicket.model.IModel;
+import org.orienteer.core.component.property.DisplayMode;
+
+import org.orienteer.core.util.ODocumentChoiceProvider;
+import org.wicketstuff.select2.*;
+
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+/**
+ * {@link IVisualizer} to show links as tags control
+ */
+public class TagsVisualizer extends AbstractSimpleVisualizer {
+    public TagsVisualizer() {
+        super("tags", false, OType.LINK, OType.LINKLIST, OType.LINKSET);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <V> Component createComponent(String id, DisplayMode mode,
+                                         IModel<ODocument> documentModel, IModel<OProperty> propertyModel, IModel<V> valueModel) {
+        if (DisplayMode.EDIT.equals(mode)) {
+            OProperty property = propertyModel.getObject();
+            OClass oClass = property.getLinkedClass();
+            if (property.getType().isMultiValue()) {
+                return new Select2MultiChoice<ODocument>(id, (IModel<Collection<ODocument>>) valueModel, new ODocumentChoiceProvider(oClass.getName(), property.getName()));
+            } else {
+                return new Select2Choice<ODocument>(id, (IModel<ODocument>) valueModel, new ODocumentChoiceProvider(oClass.getName(), property.getName()));
+            }
+        } else {
+            return null;
+        }
+    }
+
+}
---
+++ orienteer-core/src/main/java/org/orienteer/core/util/ODocumentChoiceProvider.java
@@ -0,0 +1,77 @@
+package org.orienteer.core.util;
+
+import com.orientechnologies.orient.core.id.ORecordId;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.string.Strings;
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.service.IOClassIntrospector;
+import org.wicketstuff.select2.ChoiceProvider;
+import org.wicketstuff.select2.Response;
+import ru.ydn.wicket.wicketorientdb.model.ODocumentPropertyModel;
+import ru.ydn.wicket.wicketorientdb.model.OQueryModel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Choice provider for Select2 control
+ */
+public class ODocumentChoiceProvider extends ChoiceProvider<ODocument> {
+
+    private String className;
+    private String propertyName;
+
+    private transient IOClassIntrospector oClassIntrospector;
+
+    public ODocumentChoiceProvider(String className, String propertyName) {
+        this.className = className;
+        this.propertyName = propertyName;
+    }
+
+    protected IOClassIntrospector getOClassIntrospector() {
+        if (oClassIntrospector == null) {
+            oClassIntrospector = OrienteerWebApplication.get().getOClassIntrospector();
+        }
+        return oClassIntrospector;
+    }
+
+    @Override
+    public String getDisplayValue(ODocument document) {
+        return getOClassIntrospector().getDocumentName(document);
+    }
+
+    @Override
+    public String getIdValue(ODocument document) {
+        return document.getIdentity().toString();
+    }
+
+    @Override
+    public void query(String query, int i, Response<ODocument> response) {
+        StringBuilder sql = new StringBuilder("SELECT FROM ").append(className);
+        if (!Strings.isEmpty(query)) {
+            sql.append(" WHERE ")
+                    .append(propertyName)
+                    .append(" CONTAINSTEXT :query");
+        }
+        sql.append(" LIMIT 20");
+        OQueryModel<ODocument> choicesModel = new OQueryModel<ODocument>(sql.toString());
+        choicesModel.setParameter("query", new Model<String>(query));
+        response.addAll(choicesModel.getObject());
+    }
+
+    @Override
+    public Collection<ODocument> toChoices(Collection<String> ids) {
+        ArrayList<ODocument> documents = new ArrayList<ODocument>();
+        for (String id : ids) {
+            ORecordId rid = new ORecordId(id);
+            ODocument ret = rid.getRecord();
+            documents.add(ret);
+        }
+        return documents;
+    }
+}
