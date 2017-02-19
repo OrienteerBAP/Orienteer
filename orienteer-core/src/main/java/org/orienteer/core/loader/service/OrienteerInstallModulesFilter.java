7f802983e3cab9a81147e6a35bac6e2b420b387f
weaxme <weaxme@gmail.com>
2017-02-19 18:35:20 +0200

    Save

--- orienteer-core/src/main/java/org/orienteer/core/loader/service/OrienteerInstallModulesFilter.java
+++
@@ -1,9 +0,0 @@
-package org.orienteer.core.loader.service;
-
-import org.apache.wicket.protocol.http.WicketFilter;
-
-/**
- * @author Vitaliy Gonchar
- */
-public class OrienteerInstallModulesFilter extends WicketFilter {
-}
