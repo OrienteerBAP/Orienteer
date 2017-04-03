6c5167633ecf8978d9cd3de3fcaa935029c32345
Vitaliy Gonchar <weaxme@gmail.com>
2017-04-03 04:20:00 +0300

    Add simple sql filter to list all class documents

--- orienteer-core/src/main/java/org/orienteer/core/component/widget/browse/ListAllODocumentsWidget.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/widget/browse/ListAllODocumentsWidget.java
@@ -1,29 +1,23 @@
 package org.orienteer.core.component.widget.browse;
 
+import com.google.inject.Inject;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.orienteer.core.component.FAIcon;
 import org.orienteer.core.component.FAIconType;
-import org.orienteer.core.component.command.CopyODocumentCommand;
-import org.orienteer.core.component.command.CreateODocumentCommand;
-import org.orienteer.core.component.command.DeleteODocumentCommand;
-import org.orienteer.core.component.command.EditODocumentsCommand;
-import org.orienteer.core.component.command.ExportCommand;
-import org.orienteer.core.component.command.SaveODocumentsCommand;
+import org.orienteer.core.component.command.*;
 import org.orienteer.core.component.property.DisplayMode;
 import org.orienteer.core.component.table.OrienteerDataTable;
+import org.orienteer.core.component.table.filter.OrientDbSqlDataProvider;
 import org.orienteer.core.service.IOClassIntrospector;
 import org.orienteer.core.widget.AbstractWidget;
 import org.orienteer.core.widget.Widget;
-
 import ru.ydn.wicket.wicketorientdb.model.OClassNamingModel;
-import ru.ydn.wicket.wicketorientdb.model.OQueryDataProvider;
-
-import com.google.inject.Inject;
-import com.orientechnologies.orient.core.metadata.schema.OClass;
-import com.orientechnologies.orient.core.record.impl.ODocument;
 
 /**
  * Widget to list all documents of a class
@@ -40,13 +34,20 @@ public class ListAllODocumentsWidget extends AbstractWidget<OClass> {
 		IModel<DisplayMode> modeModel = DisplayMode.VIEW.asModel();
 		
 		Form<ODocument> form = new Form<ODocument>("form");
-		OQueryDataProvider<ODocument> provider = new OQueryDataProvider<ODocument>("select from "+getModelObject().getName());
+		final OrientDbSqlDataProvider<ODocument> provider = new OrientDbSqlDataProvider<>("select from "+getModelObject().getName());
 		oClassIntrospector.defineDefaultSorting(provider, getModelObject());
 		OrienteerDataTable<ODocument, String> table = 
 				new OrienteerDataTable<ODocument, String>("table", oClassIntrospector.getColumnsFor(getModelObject(), true, modeModel), provider, 20);
+		table.setFilter(provider, modeModel);
 		table.addCommand(new CreateODocumentCommand(table, getModel()));
 		table.addCommand(new EditODocumentsCommand(table, modeModel, getModel()));
-		table.addCommand(new SaveODocumentsCommand(table, modeModel));
+		table.addCommand(new SaveODocumentsCommand(table, modeModel) {
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				provider.updateDataProvider();
+				super.onClick(target);
+			}
+		});
 		table.addCommand(new CopyODocumentCommand(table, getModel()));
 		table.addCommand(new DeleteODocumentCommand(table, getModel()));
 		table.addCommand(new ExportCommand<ODocument>(table, new PropertyModel<String>(model, "name")));
