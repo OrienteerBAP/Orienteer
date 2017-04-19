5642dea7eca66e09ae79fc933d7aecc33ea41a29
Vitaliy Gonchar <weaxme@gmail.com>
2017-04-20 00:32:11 +0300

    Add creating table without filter

--- orienteer-core/src/main/java/org/orienteer/core/component/table/component/GenericTablePanel.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/table/component/GenericTablePanel.java
@@ -3,7 +3,9 @@ package org.orienteer.core.component.table.component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxFallbackButton;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.IFilterStateLocator;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
@@ -25,28 +27,47 @@ public class GenericTablePanel<K> extends Panel {
 
     private final OrienteerDataTable<K, String> dataTable;
 
+
+
     public GenericTablePanel(String id, List<? extends IColumn<K, String>> columns, AbstractFilteredDataProvider<K> provider, int rowsPerRange) {
+       this(id, columns, provider, rowsPerRange, true);
+    }
+
+    public GenericTablePanel(String id, List<? extends IColumn<K, String>> columns, ISortableDataProvider<K, String> provider, int rowsPerRange) {
+        this(id, columns, provider, rowsPerRange, provider instanceof AbstractFilteredDataProvider);
+    }
+
+    @SuppressWarnings("unchecked")
+    private GenericTablePanel(String id, List<? extends IColumn<K, String>> columns,
+                              ISortableDataProvider<K, String> provider, int rowsPerRange, boolean filtered) {
         super(id);
-        setOutputMarkupPlaceholderTag(true);
         Args.notNull(columns, "columns");
         Args.notNull(provider, "provider");
-        FilterForm<IODataFilter<K, String>> filterForm = new FilterForm<IODataFilter<K, String>>("form", provider) {
-            @Override
-            protected void onSubmit() {
-                AjaxRequestTarget target = RequestCycle.get().find(AjaxRequestTarget.class);
-                target.add(dataTable);
-            }
-        };
-        filterForm.setOutputMarkupPlaceholderTag(true);
+        setOutputMarkupPlaceholderTag(true);
         dataTable = new OrienteerDataTable<>("table", columns, provider, rowsPerRange);
-        dataTable.addFilterForm(filterForm);
-        filterForm.add(dataTable);
-        AjaxFallbackButton button = new AjaxFallbackButton("submit", filterForm) {};
-        filterForm.setDefaultButton(button);
-        filterForm.enableFocusTracking(button);
-        filterForm.add(button);
-        filterForm.add(dataTable);
-        add(filterForm);
+        if (filtered) {
+            FilterForm<IODataFilter<K, String>> filterForm = new FilterForm<IODataFilter<K, String>>("form", (IFilterStateLocator<IODataFilter<K, String>>) provider) {
+                @Override
+                protected void onSubmit() {
+                    AjaxRequestTarget target = RequestCycle.get().find(AjaxRequestTarget.class);
+                    target.add(dataTable);
+                }
+            };
+            filterForm.setOutputMarkupPlaceholderTag(true);
+            dataTable.addFilterForm(filterForm);
+            filterForm.add(dataTable);
+            AjaxFallbackButton button = new AjaxFallbackButton("submit", filterForm) {};
+            filterForm.setDefaultButton(button);
+            filterForm.enableFocusTracking(button);
+            filterForm.add(button);
+            filterForm.add(dataTable);
+            add(filterForm);
+        } else {
+            Form form = new Form("form");
+            form.add(dataTable);
+            form.add(new AjaxFallbackButton("submit", form) {}.setVisible(false));
+            add(form);
+        }
         add(new EmptyPanel("error").setVisible(false));
     }
 
@@ -56,6 +77,7 @@ public class GenericTablePanel<K> extends Panel {
         Form form = new Form("form");
         form.add(new EmptyPanel("table").setVisible(false));
         form.setVisible(false);
+        form.add(new AjaxFallbackButton("submit", form) {}.setVisible(false));
         add(new Label("error", errorMessage));
         add(form);
     }
