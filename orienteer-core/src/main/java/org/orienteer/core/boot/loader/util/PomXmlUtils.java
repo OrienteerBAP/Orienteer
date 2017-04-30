d224c520aa7e431f186672d60e2a11660e38288b
Vitaliy Gonchar <weaxme@gmail.com>
2017-04-30 23:04:47 +0300

    Optimize imports

--- orienteer-core/src/main/java/org/orienteer/core/boot/loader/util/PomXmlUtils.java
+++ orienteer-core/src/main/java/org/orienteer/core/boot/loader/util/PomXmlUtils.java
@@ -12,15 +12,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Map;
