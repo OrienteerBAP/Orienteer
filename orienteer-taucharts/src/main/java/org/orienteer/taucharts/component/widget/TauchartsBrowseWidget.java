302ec4167733aa2dd299dfe7e5d332388d3a7c2f
SergeyAsm <barm@mail.ru>
2017-07-05 10:16:57 +0300

    Basic taucharts(www.taucharts.com) orienteer implementation

---
+++ orienteer-taucharts/src/main/java/org/orienteer/taucharts/component/widget/TauchartsBrowseWidget.java
@@ -0,0 +1,50 @@
+package org.orienteer.taucharts.component.widget;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.wicket.model.IModel;
+import org.orienteer.core.widget.Widget;
+import org.orienteer.taucharts.component.TauchartsConfig;
+import org.orienteer.taucharts.component.TauchartsPanel;
+
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+/**
+ * Widget displays {@link TauchartsPanel} on document list 
+ */
+@Widget(id="taucharts-browse", domain="browse", order=10, autoEnable=false,oClass=AbstractTauchartsWidget.WIDGET_OCLASS_NAME)
+public class TauchartsBrowseWidget extends AbstractTauchartsWidget<OClass>{
+	private static final long serialVersionUID = 1L;
+
+	public TauchartsBrowseWidget(String id, IModel<OClass> model, IModel<ODocument> widgetDocumentModel) {
+		super(id, model, widgetDocumentModel);
+	}
+	
+	@Override
+	protected void makeChartPanel(){
+		List<String> plugins = new ArrayList<String>();
+		Set<ODocument> pluginsLinks = getWidgetDocument().field(PLUGINS_PROPERTY_NAME);
+		if (pluginsLinks!=null){
+			for (ODocument oDocument : pluginsLinks) {
+				plugins.add((String) oDocument.field("alias"));
+			}
+		}
+		add(new TauchartsPanel(
+				"tauchart",
+				new TauchartsConfig(
+					(String)(((ODocument) getWidgetDocument().field(TYPE_PROPERTY_NAME)).field("alias")),
+					(String)getWidgetDocument().field(X_PROPERTY_NAME),
+					(String)getWidgetDocument().field(Y_PROPERTY_NAME),
+					(String)getWidgetDocument().field(COLOR_PROPERTY_NAME),
+					plugins,
+					(String)getWidgetDocument().field(QUERY_PROPERTY_NAME),
+					(String) getWidgetDocument().field(X_LABEL_PROPERTY_NAME),
+					(String) getWidgetDocument().field(Y_LABEL_PROPERTY_NAME)
+				)
+		));		
+	}
+
+}
---
+++ orienteer-taucharts/src/main/java/org/orienteer/taucharts/component/widget/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 
+ */
+package org.orienteer.taucharts.component.widget;
