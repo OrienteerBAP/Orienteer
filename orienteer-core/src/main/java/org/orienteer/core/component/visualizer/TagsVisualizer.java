de84dee1f747efe46e9cb06140e2a0a90e139f1f
Ilia Naryzhny <phantom@ydn.ru>
2017-07-10 10:08:10 +0300

    Fix NPE in TagsVisualizer if linked class was not specified

--- orienteer-core/src/main/java/org/orienteer/core/component/visualizer/TagsVisualizer.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/visualizer/TagsVisualizer.java
@@ -9,6 +9,8 @@ import org.orienteer.core.component.meta.OClassMetaPanel;
 import org.orienteer.core.component.property.DisplayMode;
 
 import org.orienteer.core.util.ODocumentChoiceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wicketstuff.select2.*;
 
 import com.orientechnologies.orient.core.metadata.schema.OProperty;
@@ -19,6 +21,8 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
  * {@link IVisualizer} to show links as tags control
  */
 public class TagsVisualizer extends AbstractSimpleVisualizer {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TagsVisualizer.class);
 	
     public TagsVisualizer() {
         super("suggest", false, OType.LINK, OType.LINKLIST, OType.LINKSET);
@@ -31,17 +35,21 @@ public class TagsVisualizer extends AbstractSimpleVisualizer {
         if (DisplayMode.EDIT.equals(mode)) {
             OProperty property = propertyModel.getObject();
             OClass oClass = property.getLinkedClass();
-            AbstractSelect2Choice<?, ?> choice = property.getType().isMultiValue() ?
-            		new Select2MultiChoice<ODocument>(id, (IModel<Collection<ODocument>>) valueModel, new ODocumentChoiceProvider(oClass))
-            		: new Select2Choice<ODocument>(id, (IModel<ODocument>) valueModel, new ODocumentChoiceProvider(oClass));
-            choice.getSettings()
-						.setWidth("100%")
-						.setCloseOnSelect(true)
-						.setTheme(OClassMetaPanel.BOOTSTRAP_SELECT2_THEME);
-			return choice;
-        } else {
-            return null;
+            if(oClass!=null) {
+	            AbstractSelect2Choice<?, ?> choice = property.getType().isMultiValue() ?
+	            		new Select2MultiChoice<ODocument>(id, (IModel<Collection<ODocument>>) valueModel, new ODocumentChoiceProvider(oClass))
+	            		: new Select2Choice<ODocument>(id, (IModel<ODocument>) valueModel, new ODocumentChoiceProvider(oClass));
+	            choice.getSettings()
+							.setWidth("100%")
+							.setCloseOnSelect(true)
+							.setTheme(OClassMetaPanel.BOOTSTRAP_SELECT2_THEME);
+				return choice;
+            } else {
+            	LOG.warn("Property '"+property.getFullName()+"' doesn't have linked class specified.");
+            }
         }
+        
+        return null;
     }
 
 }
