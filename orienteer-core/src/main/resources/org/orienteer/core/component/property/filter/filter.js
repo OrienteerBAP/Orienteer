e5c8e9f813da34fc830e2fe86cc9ea705c377251
Vitaliy Gonchar <weaxme@gmail.com>
2017-07-12 21:21:04 +0300

    Set z-index for draggable content

--- orienteer-core/src/main/resources/org/orienteer/core/component/property/filter/filter.js
+++ orienteer-core/src/main/resources/org/orienteer/core/component/property/filter/filter.js
@@ -1,14 +1,22 @@
 
 'use strict';
 
+var currentZindex = 1000;
+var maxZindex     = currentZindex + 100000;
+
 function showTab(id) {
     $("#" + id).tab('show');
 }
 
 function enableDraggable(id) {
-    $('#' + id).draggable({
+    var draggable = $('#' + id).draggable({
         cursor: "move",
         containment: "document",
-        zIndex: 100000
+        zIndex: maxZindex,
+        stop: function(event, ui) {
+            if (currentZindex === maxZindex) currentZindex = 1000;
+            else currentZindex++;
+            draggable.css("z-index", currentZindex);
+        }
     });
 }
