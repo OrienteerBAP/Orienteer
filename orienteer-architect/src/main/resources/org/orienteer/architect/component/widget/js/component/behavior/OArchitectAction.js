95f3214220ec27128d4aeef1c2d2ad59bea807d8
Vitaliy Gonchar <weaxme@gmail.com>
2017-09-19 19:19:20 +0300

    Add lock for delete property when user edit current class

--- orienteer-architect/src/main/resources/org/orienteer/architect/component/widget/js/component/behavior/OArchitectAction.js
+++ orienteer-architect/src/main/resources/org/orienteer/architect/component/widget/js/component/behavior/OArchitectAction.js
@@ -257,7 +257,8 @@ var OArchitectAction = {
         function getPreparedCells(cells) {
             var result = [];
             OArchitectUtil.forEach(cells, function (cell) {
-                if (OArchitectUtil.isCellDeletable(cell)) {
+                if (OArchitectUtil.isCellDeletable(cell) && cell.value instanceof OArchitectOClass
+                    && OArchitectAction.isAvailableActionForClass(cell.value)) {
                     cell.value.removed = true;
                     result.push(cell);
                 }
@@ -271,18 +272,20 @@ var OArchitectAction = {
      */
     deleteOPropertyAction: function (editor, cell) {
         if (cell != null && cell.value instanceof OArchitectOProperty) {
-            var graph = editor.graph;
-            graph.stopEditing(false);
-            var oClassCell = OArchitectUtil.getClassCellByPropertyCell(cell);
-            var oClass = oClassCell.value;
-            var property = cell.value;
-            graph.getModel().beginUpdate();
-            try {
-                oClass.saveState();
-                oClass.removeProperty(property, false);
-                oClass.updateValueInCell();
-            } finally {
-                graph.getModel().endUpdate();
+            if (OArchitectAction.isAvailableActionForClass(cell.value.ownerClass)) {
+                var graph = editor.graph;
+                graph.stopEditing(false);
+                var oClassCell = OArchitectUtil.getClassCellByPropertyCell(cell);
+                var oClass = oClassCell.value;
+                var property = cell.value;
+                graph.getModel().beginUpdate();
+                try {
+                    oClass.saveState();
+                    oClass.removeProperty(property, false);
+                    oClass.updateValueInCell();
+                } finally {
+                    graph.getModel().endUpdate();
+                }
             }
         }
     },
