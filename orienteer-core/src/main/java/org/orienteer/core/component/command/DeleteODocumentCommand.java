31d4563d94565d0b3c3c796555a2106834139356
Vitaliy Gonchar <weaxme@gmail.com>
2018-04-12 16:57:31 +0300

    Reload metadata from root

--- orienteer-core/src/main/java/org/orienteer/core/component/command/DeleteODocumentCommand.java
+++ orienteer-core/src/main/java/org/orienteer/core/component/command/DeleteODocumentCommand.java
@@ -1,31 +1,18 @@
 package org.orienteer.core.component.command;
 
-import java.util.List;
-
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.event.Broadcast;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.model.ResourceModel;
-import org.orienteer.core.component.BootstrapType;
-import org.orienteer.core.component.FAIconType;
-import org.orienteer.core.component.table.CheckBoxColumn;
-import org.orienteer.core.component.table.DataTableCommandsToolbar;
-import org.orienteer.core.component.table.OrienteerDataTable;
-
-import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
-import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import com.orientechnologies.orient.core.record.impl.ODocument;
-
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.model.IModel;
+import org.orienteer.core.component.table.OrienteerDataTable;
 import ru.ydn.wicket.wicketorientdb.model.OClassModel;
 import ru.ydn.wicket.wicketorientdb.security.ISecuredComponent;
 import ru.ydn.wicket.wicketorientdb.security.OSecurityHelper;
 import ru.ydn.wicket.wicketorientdb.security.OrientPermission;
 import ru.ydn.wicket.wicketorientdb.security.RequiredOrientResource;
+import ru.ydn.wicket.wicketorientdb.utils.DBClosure;
+
+import java.util.List;
 
 /**
  * {@link Command} to delete an {@link ODocument}
@@ -49,10 +36,11 @@ public class DeleteODocumentCommand extends AbstractDeleteCommand<ODocument>  im
 	@Override
 	protected void performMultiAction(AjaxRequestTarget target, List<ODocument> objects) {
 		super.performMultiAction(target, objects);
-		ODatabaseDocument db = getDatabase();
-		db.commit(true);
-		db.begin();
-		db.getMetadata().reload();
+		DBClosure.sudoConsumer(db -> {
+			db.commit(true);
+			db.begin();
+			db.getMetadata().reload();
+		});
 	}
 
 	@Override
