274c1a88075b469d8936fa69aa9dc6a28ff1b65c
Vitaliy Gonchar <weaxme@gmail.com>
2018-05-09 13:07:03 +0300

    Refactor for methods
    1. Fixed search widget component
    2. Use getModel() for set main object model in initialization methods in
    AbstractWidget

--- orienteer-core/src/main/java/org/orienteer/core/method/OMethodsManager.java
+++ orienteer-core/src/main/java/org/orienteer/core/method/OMethodsManager.java
@@ -1,26 +1,20 @@
 package org.orienteer.core.method;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import org.apache.wicket.Component;
+import org.apache.wicket.model.IModel;
+import org.orienteer.core.component.BootstrapType;
+import org.orienteer.core.component.ICommandsSupportComponent;
+import org.orienteer.core.component.command.Command;
+import org.orienteer.core.method.definitions.JavaClassOMethodDefinitionStorage;
+import org.orienteer.core.method.definitions.JavaMethodOMethodDefinitionStorage;
+import org.orienteer.core.module.IOrienteerModule;
+import org.orienteer.core.widget.AbstractWidget;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.wicket.Component;
-import org.apache.wicket.model.IModel;
-import org.orienteer.core.method.definitions.JavaMethodOMethodDefinitionStorage;
-import org.orienteer.core.component.BootstrapType;
-import org.orienteer.core.component.IBootstrapAware;
-import org.orienteer.core.component.ICommandsSupportComponent;
-import org.orienteer.core.component.command.Command;
-import org.orienteer.core.method.definitions.JavaClassOMethodDefinitionStorage;
-import org.orienteer.core.module.IOrienteerModule;
-import org.orienteer.core.widget.AbstractWidget;
-
-import com.google.common.collect.TreeMultiset;
 
 /**
  * 
@@ -76,7 +70,7 @@ public class OMethodsManager {
 				.sorted((c1, c2) -> {
 							int ret = Integer.compare(c1.getOrder(), c2.getOrder());
 							if(ret==0) ret=c1.getMethodId().compareTo(c2.getMethodId());
-							return ret; 
+							return ret;
 						})
 				.map(c -> c.getMethod(context))
 				.filter(Objects::nonNull)
@@ -96,13 +90,15 @@ public class OMethodsManager {
 									Component relatedComponent, BootstrapType bootstrapType) {
 		populateInternal(commandSupport, place, mainObjectModel, relatedComponent, bootstrapType, true);
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	private <T> void populateInternal(ICommandsSupportComponent<T> commandSupport, MethodPlace place, 
 										IModel<?> mainObjectModel, Component relatedComponent, 
 										BootstrapType bootstrapType, boolean overrideBootstrapType) {
-		AbstractWidget<?> widget = commandSupport.getComponent().findParent(AbstractWidget.class);
-		List<IMethod> methods = getMethods(new MethodContext(mainObjectModel,widget,place,relatedComponent));
-		for ( IMethod method : methods) {
+		AbstractWidget<?> widget = getWidget(commandSupport);
+		MethodContext ctx = new MethodContext(mainObjectModel, widget, place, relatedComponent);
+		List<IMethod> methods = getMethods(ctx);
+		for (IMethod method : methods) {
 			Command<T> component = (Command<T>) method.createCommand(commandSupport.newCommandId()); 
 			if (component !=null){
 				if (overrideBootstrapType){
@@ -113,4 +109,11 @@ public class OMethodsManager {
 		}
 	}
 
+	private AbstractWidget<?> getWidget(ICommandsSupportComponent<?> commandSupport) {
+		Component component = commandSupport.getComponent();
+		if (component instanceof AbstractWidget) {
+			return (AbstractWidget<?>) component;
+		}
+		return component.findParent(AbstractWidget.class);
+	}
 }
