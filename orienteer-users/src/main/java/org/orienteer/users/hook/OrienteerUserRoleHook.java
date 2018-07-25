956cc7356be970525d408ce4d3332d8fd8b0790a
Vitaliy Gonchar <weaxme@gmail.com>
2018-07-25 11:20:45 +0300

    Added javadocs

--- orienteer-users/src/main/java/org/orienteer/users/hook/OrienteerUserRoleHook.java
+++ orienteer-users/src/main/java/org/orienteer/users/hook/OrienteerUserRoleHook.java
@@ -9,6 +9,10 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/**
+ * Hook for update field _allowRead in role document before create role
+ * Need for allow role read herself by default
+ */
 public class OrienteerUserRoleHook extends ODocumentHookAbstract {
 
     public OrienteerUserRoleHook(ODatabaseDocument database) {
@@ -16,6 +20,11 @@ public class OrienteerUserRoleHook extends ODocumentHookAbstract {
         setIncludeClasses(ORole.CLASS_NAME);
     }
 
+    /**
+     * Add doc to field _allowRead if it doesn't exists in _allowRead set
+     * @param doc {@link ODocument} role document
+     * @return {@link com.orientechnologies.orient.core.hook.ORecordHook.RESULT} returns super.onBeforeCreate(doc)
+     */
     @Override
     public RESULT onRecordBeforeCreate(ODocument doc) {
         Set<ODocument> allowRead = doc.field(ORestrictedOperation.ALLOW_READ.getFieldName(), Set.class);
