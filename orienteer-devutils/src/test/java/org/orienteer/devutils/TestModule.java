35b7b4ba76da0b532359feb657c4e5304b272d25
Ilia Naryzhny <phantom@ydn.ru>
2018-11-11 18:02:51 -0800

    Add widget to tools page to monitor key metrics

--- orienteer-devutils/src/test/java/org/orienteer/devutils/TestModule.java
+++ orienteer-devutils/src/test/java/org/orienteer/devutils/TestModule.java
@@ -2,11 +2,13 @@ package org.orienteer.devutils;
 
 import org.orienteer.core.OrienteerWebApplication;
 import org.orienteer.core.module.IOrienteerModule;
-
+import org.orienteer.devutils.web.ToolsPage;
 import org.orienteer.junit.OrienteerTestRunner;
 import org.orienteer.junit.OrienteerTester;
 
 import static org.junit.Assert.*;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -41,5 +43,14 @@ public class TestModule
 		ScriptExecutor se = ScriptExecutorHolder.get().getScriptExecutor();
 		assertEquals(tester.getSchema().getClass("OUser").count(), 
 				(Object)  ((OResultSet<ODocument>)se.executeWithoutHistory("SELECT count(1) from OUser","SQL",null).getResult()).get(0).field("count"));
+	}
+	
+	@Test
+	public void testPageLoad() {
+		tester.signIn("admin", "admin");
+		tester.startPage(ToolsPage.class, new PageParameters().add("tab", "console"));
+		tester.assertRenderedPage(ToolsPage.class);
+		tester.startPage(ToolsPage.class, new PageParameters().add("tab", "monitoring"));
+		tester.assertRenderedPage(ToolsPage.class);
 	}
 }
