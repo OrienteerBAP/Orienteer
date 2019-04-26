6f797b4907a3d889639d94c75eba7c8272f463b7
Vitaliy Gonchar <weaxme@gmail.com>
2019-04-26 12:51:32 +0300

    If OAuth2 is not active - show default login panel
    Issue #721

--- orienteer-users/src/main/java/org/orienteer/users/web/OUsersLoginPage.java
+++ orienteer-users/src/main/java/org/orienteer/users/web/OUsersLoginPage.java
@@ -7,7 +7,9 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.orienteer.core.MountPath;
 import org.orienteer.core.web.LoginPage;
 import org.orienteer.users.component.OUsersLoginPanel;
+import org.orienteer.users.module.OrienteerUsersModule;
 import org.orienteer.users.repository.OAuth2Repository;
+import org.orienteer.users.repository.OrienteerUserModuleRepository;
 import org.orienteer.users.service.IOAuth2Service;
 import ru.ydn.wicket.wicketorientdb.utils.DBClosure;
 
@@ -26,25 +28,34 @@ public class OUsersLoginPage extends LoginPage {
 
     public OUsersLoginPage(PageParameters params) {
         this();
-        String code = params.get("code").toOptionalString();
-        String state = params.get("state").toOptionalString();
 
-        if (!Strings.isNullOrEmpty(code) && !Strings.isNullOrEmpty(state)) {
-            OAuth2Repository.getServiceContextByState(state)
-                    .ifPresent(ctx -> {
-                        boolean authorized = authService.authorize(ctx.getService(), code);
-                        ctx.setUsed(true);
-                        DBClosure.sudoConsumer(db -> ctx.save());
-                        if (authorized) {
-                            continueToOriginalDestination();
-                            setResponsePage(getApplication().getHomePage());
-                        }
-                    });
+        if (isOAuth2Active()) {
+            String code = params.get("code").toOptionalString();
+            String state = params.get("state").toOptionalString();
+
+            if (!Strings.isNullOrEmpty(code) && !Strings.isNullOrEmpty(state)) {
+                OAuth2Repository.getServiceContextByState(state)
+                        .ifPresent(ctx -> {
+                            boolean authorized = authService.authorize(ctx.getService(), code);
+                            ctx.setUsed(true);
+                            DBClosure.sudoConsumer(db -> ctx.save());
+                            if (authorized) {
+                                continueToOriginalDestination();
+                                setResponsePage(getApplication().getHomePage());
+                            }
+                        });
+            }
         }
     }
 
     @Override
     protected WebMarkupContainer createLoginPanel(String id) {
-        return new OUsersLoginPanel(id);
+        return isOAuth2Active() ? new OUsersLoginPanel(id) : super.createLoginPanel(id);
+    }
+
+    private boolean isOAuth2Active() {
+        return OrienteerUserModuleRepository.getModuleModel()
+                .map(OrienteerUsersModule.ModuleModel::isOAuth2)
+                .orElseThrow(() -> new IllegalStateException("There is no configured module " + OrienteerUsersModule.ModuleModel.CLASS_NAME));
     }
 }
