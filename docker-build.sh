978677fe688ea4af81a983b0862b51eda87c40af
Vitalii Honchar <weaxme@gmail.com>
2020-01-24 13:19:08 +0200

    Deleted docker-build.sh

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mvn -pl !orienteer-archetype-war,!orienteer-archetype-jar,!orienteer-standalone,!orienteer-birt,!orienteer-bpm,!orienteer-camel,!orienteer-etl,!orienteer-taucharts,!orienteer-architect clean install
-docker build .
