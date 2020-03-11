81b90fee5213870b1eb6810d2140d09b4a7fec42
Ilia Naryzhny <phantom@ydn.ru>
2020-03-10 20:34:07 -0700

    Initial commit of orienteer-metrics for issue #395

---
+++ orienteer-metrics/src/main/java/org/orienteer/metrics/OMetricsModule.java
@@ -0,0 +1,43 @@
+package org.orienteer.metrics;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.AbstractOrienteerModule;
+import org.orienteer.core.module.IOrienteerModule;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.hotspot.DefaultExports;
+
+/**
+ * {@link IOrienteerModule} for 'metrics' module
+ */
+public class OMetricsModule extends AbstractOrienteerModule{
+	
+	
+	protected OMetricsModule() {
+		super("metrics", 1);
+	}
+	
+	@Override
+	public ODocument onInstall(OrienteerWebApplication app, ODatabaseDocument db) {
+		super.onInstall(app, db);
+		return null;
+	}
+	
+	@Override
+	public void onInitialize(OrienteerWebApplication app, ODatabaseDocument db) {
+		super.onInitialize(app, db);
+		DefaultExports.initialize();
+		app.mountPackage(OMetricsModule.class.getPackage().getName());
+	}
+	
+	@Override
+	public void onDestroy(OrienteerWebApplication app, ODatabaseDocument db) {
+		app.unmountPackage(OMetricsModule.class.getPackage().getName());
+		CollectorRegistry.defaultRegistry.clear();
+		super.onDestroy(app, db);
+	}
+
+}
---
+++ orienteer-metrics/src/main/java/org/orienteer/metrics/OMetricsResource.java
@@ -0,0 +1,53 @@
+package org.orienteer.metrics;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.AbstractResource;
+import org.apache.wicket.request.resource.IResource;
+import org.apache.wicket.util.time.Time;
+import org.orienteer.core.MountPath;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.common.TextFormat;
+
+/**
+ * Resource to share metrics 
+ */
+@MountPath("/metrics")
+public class OMetricsResource extends AbstractResource {
+
+	@Override
+	protected ResourceResponse newResourceResponse(Attributes attributes) {
+		ResourceResponse response = new ResourceResponse();
+        response.setLastModified(Time.now());
+//        response.setStatusCode(HttpServlet);
+        response.setContentType(TextFormat.CONTENT_TYPE_004);
+        if (response.dataNeedsToBeWritten(attributes)) {
+        	
+        	PageParameters params = attributes.getParameters();
+        	Set<String> includedMetrics = new HashSet<>();
+        	params.getValues("name").forEach((sv) -> {if(!sv.isEmpty()) includedMetrics.add(sv.toString()); });
+        	
+            response.setWriteCallback(createWriteCallback(CollectorRegistry.defaultRegistry, includedMetrics));
+        }
+        return response;
+	}
+	
+	private WriteCallback createWriteCallback(CollectorRegistry registry, Set<String> metrics) {
+        return new WriteCallback() {
+            @Override
+            public void writeData(IResource.Attributes attributes) throws IOException {
+				try(OutputStreamWriter writer = new OutputStreamWriter(attributes.getResponse().getOutputStream(), "UTF8")) {
+					TextFormat.write004(writer, registry.filteredMetricFamilySamples(metrics));
+					writer.flush();
+				}
+            }
+        };
+    }
+
+}
