aa8c95d957051779f3ac504fbb332c846ba5de2b
Vitalii Honchar <weaxme@gmail.com>
2020-04-02 14:49:05 +0300

    Added method IODocumentWrapper.get

--- orienteer-core/src/main/java/org/orienteer/core/dao/IODocumentWrapper.java
+++ orienteer-core/src/main/java/org/orienteer/core/dao/IODocumentWrapper.java
@@ -2,6 +2,7 @@ package org.orienteer.core.dao;
 
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.type.ODocumentWrapper;
+import org.orienteer.core.OrienteerWebApplication;
 
 import java.io.Serializable;
 
@@ -9,7 +10,17 @@ import java.io.Serializable;
  * Interface which mirror methods of {@link ODocumentWrapper} 
  */
 public interface IODocumentWrapper extends Serializable {
-	
+
+	static <T extends IODocumentWrapper> T get(Class<T> wrapperClass) {
+		return OrienteerWebApplication.lookupApplication().getServiceInstance(wrapperClass);
+	}
+
+	static <T extends IODocumentWrapper> T get(Class<T> wrapperClass, ODocument document) {
+		T wrapper = get(wrapperClass);
+		wrapper.fromStream(document);
+		return wrapper;
+	}
+
 	void fromStream(final ODocument iDocument);
 	ODocument toStream();
 	<R extends IODocumentWrapper> R load();
