b59592dee138cf18423cab519c00f97c552200d7
Ilia Naryzhny <phantom@ydn.ru>
2020-12-01 16:43:56 -0800

    Pause transaction for OConsole

--- orienteer-devutils/src/main/java/org/orienteer/devutils/ODBConsoleEngine.java
+++ orienteer-devutils/src/main/java/org/orienteer/devutils/ODBConsoleEngine.java
@@ -7,13 +7,18 @@ import java.io.StringWriter;
 
 import org.apache.wicket.util.string.Strings;
 import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.OrienteerWebSession;
 
 import com.google.common.base.Throwables;
 import com.orientechnologies.orient.console.OConsoleDatabaseApp;
 import com.orientechnologies.orient.core.command.script.OCommandScriptException;
+import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
+import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.exception.OSecurityAccessException;
 import com.orientechnologies.orient.core.sql.OCommandSQLParsingException;
+import com.orientechnologies.orient.core.tx.OTransaction;
+import com.orientechnologies.orient.core.tx.OTransactionNoTx;
 
 import ru.ydn.wicket.wicketconsole.IScriptContext;
 import ru.ydn.wicket.wicketconsole.IScriptEngine;
@@ -36,9 +41,32 @@ public class ODBConsoleEngine implements IScriptEngine {
 			command = command.trim();
 			String[] args = command.split("(?<=\") *(?=\")");
 			final OConsoleDatabaseApp console = new OConsoleDatabaseApp(args) {
+				
+				Boolean wasInTransaction=null;
 				@Override
 				protected void onException(Throwable e) {
 					result.setError(toError(e));
+				}
+				
+				@Override
+				protected void onBefore() {
+					ODatabaseDocumentInternal db = ODatabaseRecordThreadLocal.instance().getIfDefined();
+					if(db!=null) {
+						wasInTransaction = db.getTransaction().isActive();
+						if(wasInTransaction) db.commit(true);
+					}
+					super.onBefore();
+				}
+				
+				@Override
+				protected void onAfter() {
+					super.onAfter();
+					if(wasInTransaction) {
+						ODatabaseDocumentInternal db = ODatabaseRecordThreadLocal.instance().getIfDefined();
+						if(db!=null) {
+							db.begin();
+						}
+					}
 				}
 			};
 			console.set("verbose", "1");
