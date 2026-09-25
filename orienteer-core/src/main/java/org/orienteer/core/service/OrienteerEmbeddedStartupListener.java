c0c558991896e725552ed13b5ebc77d20917a631
Ilia Naryzhny <phantom@ydn.ru>
2026-09-25 14:26:34 -0700

    Remove the dead Nashorn engine registration (no JDK ships Nashorn; GraalJS registers itself)

--- orienteer-core/src/main/java/org/orienteer/core/service/OrienteerEmbeddedStartupListener.java
+++ orienteer-core/src/main/java/org/orienteer/core/service/OrienteerEmbeddedStartupListener.java
@@ -1,18 +1,15 @@
 package org.orienteer.core.service;
 
-import com.orientechnologies.orient.core.command.script.OScriptManager;
 import com.orientechnologies.orient.core.config.OGlobalConfiguration;
 import com.orientechnologies.orient.core.db.ODatabasePool;
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
 import com.orientechnologies.orient.core.db.OrientDBConfigBuilder;
-import com.orientechnologies.orient.core.db.OrientDBInternal;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.security.OUser;
 import com.orientechnologies.orient.server.config.OServerConfiguration;
 
-import lombok.experimental.Delegate;
 import ru.ydn.wicket.wicketorientdb.EmbeddOrientDbApplicationListener;
 import ru.ydn.wicket.wicketorientdb.IOrientDbSettings;
 import ru.ydn.wicket.wicketorientdb.OrientDbSettings;
@@ -21,27 +18,11 @@ import ru.ydn.wicket.wicketorientdb.OrientDbWebApplication;
 import java.io.File;
 import java.net.URL;
 
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptEngineManager;
-
 /**
  * Listener which creates Orienteer database after server startup
  */
 public class OrienteerEmbeddedStartupListener extends EmbeddOrientDbApplicationListener {
 	
-	/**
-	 * {@link ScriptEngineFactory} to fake actual factory class name 
-	 */
-	public static class ScriptDelegateEngineFactory implements ScriptEngineFactory {
-		@Delegate
-		private ScriptEngineFactory delegate;
-		
-		public ScriptDelegateEngineFactory(ScriptEngineFactory delegate) {
-			this.delegate = delegate;
-		}
-		
-	}
-
     public OrienteerEmbeddedStartupListener() {
         super();
     }
@@ -73,16 +54,6 @@ public class OrienteerEmbeddedStartupListener extends EmbeddOrientDbApplicationL
         			.build())) {
         	try(ODatabaseSession session = orientDB.open(settings.getDbName(), OrientDbSettings.ADMIN_DEFAULT_USERNAME, OrientDbSettings.ADMIN_DEFAULT_PASSWORD)){
         		onDbCreated(session, settings);
-        	}
-        }
-        //Workaround for https://github.com/orientechnologies/orientdb/issues/9432
-        ScriptEngineManager sem = new ScriptEngineManager();
-        for(ScriptEngineFactory sef : sem.getEngineFactories()) {
-        	if(sef.getNames().contains("nashorn")) {
-        		OScriptManager scriptManager = OrientDBInternal.extract(orientDB).getScriptManager();
-        		scriptManager.registerEngine("nashorn", new ScriptDelegateEngineFactory(sef));
-        		scriptManager.registerFormatter("nashorn", scriptManager.getFormatters().get("javascript"));
-        		break;
         	}
         }
     }
